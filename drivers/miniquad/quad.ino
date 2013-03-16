4afd870132ac225512c985a73dcb7b7cf5b51bc8
karl <karl@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-03-16 09:00:34 +0000

    Updated code for mini-quad that moves fast and has a narrow dog walk and a wide crab walk.

--- drivers/miniquad/quad.ino
+++ drivers/miniquad/quad.ino
@@ -1,12 +1,13 @@
-#include <Servo.h> 
+#include <Servo.h>
 
 #define MAXPOS 10
+#define TIMESCALE 125
 
 class Leg {
   boolean invert;
   Servo hip,coxa,knee;
   public:
-  int h,c,k;
+  byte h,c,k;
   void attach(int hipPin,int coxaPin,int kneePin,boolean newInvert) {
     knee.attach(kneePin);
     coxa.attach(coxaPin);
@@ -38,28 +39,21 @@ class Leg {
 };
 
 class LegSequence {
-  int denominator,numerator;
   byte hAngle[MAXPOS];
   byte cAngle[MAXPOS];
   byte kAngle[MAXPOS];
-  long time[MAXPOS];
+  int time[MAXPOS];
   protected:
-  int pos;
+  byte pos;
   long total,offset;
   boolean reverse,interp;
   public:
-  void setSpeed(int newNumerator,int newDenominator) {
-    numerator=newNumerator;
-    denominator=newDenominator;
-  }
   virtual void init(long newOffset,boolean newReverse,boolean newInterp) {
-   pos=0; 
    total=0;
+   pos=0;
    offset=newOffset;
    reverse=newReverse;
    interp=newInterp;
-   denominator=1;
-   numerator=1;
   }
   void add(int h,int c,int k,long t) {
     if (pos==MAXPOS) return;
@@ -71,7 +65,6 @@ class LegSequence {
     pos++;
   }
   void pose(Leg &l) {
-//    long s=(((millis()+offset)*numerator)/denominator) % total;
     long s=(millis()+offset) % total;
     if (reverse) s=total-s;
     for (int i=0;i<pos;i++) {
@@ -96,30 +89,6 @@ class LegSequence {
   }
 };
 
-class FrontCrab:public LegSequence {
-  public:
-  void init(long newOffset,boolean newReverse=false,boolean newInterp=false){
-    LegSequence::init(newOffset,newReverse,newInterp);
-    add(140,90,90,1500);
-    add(110,90,90,1000);
-    add(90,90,90,1000);
-    add(90,0,0,250);
-    add(140,0,0,250);
-  }
-};
- 
-class BackCrab:public LegSequence {
-  public:
-  void init(long newOffset,boolean newReverse=false,boolean newInterp=false){
-    LegSequence::init(newOffset,newReverse,newInterp);
-    add(90,90,90,1500);
-    add(110,90,90,1000);
-    add(140,90,90,1000);
-    add(140,0,0,250);
-    add(90,0,0,250);
-  }
-};
-
 void compute2D(float x,float y,float l,float &knee,float &femur) {
   float h=sqrt(x*x+y*y);
   float theta1=atan(x/y)*180.0/M_PI;
@@ -134,13 +103,41 @@ void compute3D(float x,float y,float z,float l,float &knee,float &femur,float &h
   compute2D(h,y,l,knee,femur);
   if (invert) {
     hip=180.0-hip;
-//    =180.0-femur;
   }  
 }
 
 class Dog:public LegSequence {
   public:
-  Dog(float xstart,float xstop,float y,float z,float dy,float l,int total) {
+  Dog(float xstart,float xstop,float dy,float l,int total) {
+    pos=0;
+    boolean invert=false;
+    float femur,knee,hip;
+    int fast=total/4; // fast phase time
+    int slow=total-fast; // slow phase time
+    int dt=slow/(MAXPOS-2);
+    for (int i=0;i<MAXPOS-2;i++) {
+      float dx=(xstop-xstart)/(float)(MAXPOS-2);
+      float x=dx*(float)i+xstart;
+      compute2D(x,l,l,knee,femur);
+
+      add(115,(int)femur,(int)knee,dt);
+    }
+    compute2D(xstop,l-dy,l,knee,femur);
+    add(115,(int)femur,(int)knee,fast/2);
+    compute2D(xstart,l-dy,l,knee,femur);
+    add(115,(int)femur,(int)knee,fast/2);    
+  }
+  void init(long newOffset,boolean newReverse=false,boolean newInterp=false){
+    offset=newOffset;
+    reverse=newReverse;
+    interp=true;
+  }
+};
+
+
+class Crab:public LegSequence {
+  public:
+  Crab(float xstart,float xstop,float y,float z,float dy,float l,int total) {
     pos=0;
     boolean invert=false;
     float femur,knee,hip;
@@ -170,11 +167,30 @@ class Frozen:public LegSequence {
   public:
   void init(long newOffset,boolean newReverse=false,boolean newInterp=false){
     LegSequence::init(newOffset,newReverse,newInterp);
-    add(90,90,90,1500);
+    add(135,90,90,1500);
   }
 };
 
- 
+class FrozenPark:public LegSequence {
+  public:
+  void init(long newOffset,boolean newReverse=false,boolean newInterp=false){
+    LegSequence::init(newOffset,newReverse,newInterp);
+    add(135,35,90,1500);
+  }
+};
+
+class FrozenTall:public LegSequence {
+  public:
+  void init(long newOffset,boolean newReverse=false,boolean newInterp=false){
+    LegSequence::init(newOffset,newReverse,newInterp);
+//    add(170,170,170,1500);  // narrow in walk direction
+//    add(170,10,135,1500);   // base on ground legs up narrow
+    add(90,170,170,31000);
+    add(90,150,150,1000);
+  }
+};
+
+
 class Quad {
   LegSequence *lsrf,*lslf,*lsrb,*lslb;
   public:
@@ -195,30 +211,6 @@ class Quad {
     lb.attach(10,9,8,true);
     rb.attach(13,12,11,false);
     setSequence();
-  }
-  void change(char *change) {
-    char s=change[0];
-    char t=change[1]-'0';
-    char n=change[2]-'0';
-    n=t*10+n;
-    if (s=='u') {
-      rf.c+=n; lf.c+=n; rb.c+=n; lb.c+=n;
-    }
-    if (s=='d') {
-      rf.c-=n; lf.c-=n; rb.c-=n; lb.c-=n;
-    }
-    if (s=='r') {
-      rf.h-=n; lf.h+=n; rb.h+=n; lb.h-=n;
-    }
-    if (s=='l') {
-      rf.h+=n; lf.h-=n; rb.h-=n; lb.h+=n;
-    }
-    if (s=='o') {
-      rf.k+=n; lf.k+=n; rb.k+=n; lb.k+=n;
-    }
-    if (s=='i') {
-      rf.k-=n; lf.k-=n; rb.k-=n; lb.k-=n;
-    }    
   }
   void pose() {
       if (lsrf!=NULL && 
@@ -243,43 +235,44 @@ class Quad {
     rb.update();
     lb.update();
   }
-  void setSpeed(int num,int den) {
-    if (lsrf!=NULL && 
-        lslf!=NULL && 
-        lsrb!=NULL && 
-        lslb!=NULL) {
-      lsrf->setSpeed(num,den);
-      lslf->setSpeed(num,den);
-      lsrb->setSpeed(num,den);
-      lslb->setSpeed(num,den);
-    }
-  }
 };
 
 Quad myQuad;
-BackCrab lbc,rbc;
-FrontCrab lfc,rfc;
-Dog rbd(-.5,1.0,1.5,.7,.5,1.0,500),lbd(-.5,1.0,1.5,.7,.5,1.0,500);
-Dog rfd(1.0,-.5,1.5,.7,.5,1.0,500),lfd(1.0,-.5,1.5,.7,.5,1.0,500);
-//Frozen rfd,lfd;
-//Frozen rbd,lbd;
+Crab rb1(0.,1.,1.,.75,0.5,1.0,4*TIMESCALE),lb1(0.,1.,1.,.75,0.5,1.0,4*TIMESCALE);
+Crab rf1(1.,0.,1.,.75,0.5,1.0,4*TIMESCALE),lf1(1.,0.,1.,.75,0.5,1.0,4*TIMESCALE);
+Dog rb2(1.5,0.5,.5,1.0,4*TIMESCALE),lb2(0.5,1.5,.5,1.0,4*TIMESCALE);
+Dog rf2(1.5,0.5,.5,1.0,4*TIMESCALE),lf2(0.5,1.5,.5,1.0,4*TIMESCALE);
+Frozen rf3,lf3,rb3,lb3;
+FrozenPark rf4,lf4,rb4,lb4;
+FrozenTall rf5,lf5,rb5,lb5;
 long start;
 
 void setup() { 
   Serial.begin(9600);
   myQuad.attach();
   myQuad.park();
-//  rfc.init(0);
-//  rbc.init(1000);
-//  lfc.init(2000);
-//  lbc.init(3000);
-  rfd.init(0);
-  lbd.init(125);
-  lfd.init(250);
-  rbd.init(325);
-//  myQuad.setSequence(&rfc,&lfc,&rbc,&lbc);
-  myQuad.setSequence(&rfd,&lfd,&rbd,&lbd);
-//  myQuad.setSpeed(2,1);
+  rf1.init(0);
+  lb1.init(TIMESCALE);
+  rb1.init(2*TIMESCALE);
+  lf1.init(3*TIMESCALE);
+  rf2.init(0);
+  lb2.init(TIMESCALE);
+  rb2.init(2*TIMESCALE);
+  lf2.init(3*TIMESCALE);
+  rf3.init(1);
+  lb3.init(1);
+  rb3.init(1);
+  lf3.init(1);
+  rf4.init(1);
+  lb4.init(1);
+  rb4.init(1);
+  lf4.init(1);
+  rf5.init(0);
+  lb5.init(8000);
+  rb5.init(16000);
+  lf5.init(24000);
+
+  myQuad.setSequence(&rf3,&lf3,&rb3,&lb3);
 }
 
 void loop() { 
@@ -287,7 +280,17 @@ void loop() {
   myQuad.update();
   if (Serial.available()) {
     Serial.readBytesUntil(0x0d,command,3);
-    myQuad.change(command);
+    if (command[0]=='f') {
+      myQuad.setSequence(&rf3,&lf3,&rb3,&lb3);
+    } else if (command[0]=='d') {
+        myQuad.setSequence(&rf2,&lf2,&rb2,&lb2);
+    } else if (command[0]=='c') {
+        myQuad.setSequence(&rf1,&lf1,&rb1,&lb1);
+    } else if (command[0]=='p') {
+        myQuad.setSequence(&rf4,&lf4,&rb4,&lb4);
+    } else if (command[0]=='h') {
+        myQuad.setSequence(&rf5,&lf5,&rb5,&lb5);
+    }
   }
   long now=millis();
   myQuad.pose();
