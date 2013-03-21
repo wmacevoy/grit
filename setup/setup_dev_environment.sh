f69af3c3806d6db0d098f185735e6475d3c40237
dan <dan@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-03-21 19:19:32 +0000

    Updated the setup bash scripts.

--- setup/setup_dev_environment.sh
+++ setup/setup_dev_environment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc lib32stdc++6-4.6-dbg lib32mudflap0
+apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc 
