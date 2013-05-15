b49cf4bfc72c62225699b4e7832f7b962c435908
dan <dan@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-05-15 22:54:34 +0000

    Added bzip2 library and a test for the bson serialization.
    Former-commit-id: 65903efae1ccfb11b19d99a4ce81ad0cd824c082

--- centaur/libcentaurtest/src/CentaurMessageTestCase.cpp
+++ centaur/libcentaurtest/src/CentaurMessageTestCase.cpp
@@ -1,21 +1,43 @@
 #include "CentaurMessageTestCase.h"
 
 #include <bson/bson.h>
+#include <bzip2/bzlib.h>
+
+#include <string.h>
 
 CPPUNIT_TEST_SUITE_REGISTRATION( CentaurMessageTestCase );
 
 void CentaurMessageTestCase::testBSON()
 {
 
-	bson::bo empty;
-	cout << "empty: " << empty.hexDump() << endl;
-
-	/* make a simple { name : 'joe', age : 33.7 } object */
+	bson::bo objOrginal;
 	{
-		bson::bob b;
-		b.append("name", "joe");
-		b.append("age", 33.7);
-		cout << "b: " << b.obj().hexDump() << endl;
+		bson::bob objBuilder;
+		objBuilder.append("name", "joe");
+		objBuilder.append("age", 33.7);
+		objOrginal = objBuilder.obj();
 	}
+
+	std::vector<char> compressedData;
+	compressedData.resize(objOrginal.objsize() * 4);
+	unsigned int compressedDataSize = compressedData.size();
+	CPPUNIT_ASSERT_EQUAL(BZ_OK, BZ2_bzBuffToBuffCompress(compressedData.data(), &compressedDataSize, (char *)objOrginal.objdata(), objOrginal.objsize(), 9, 0, 30));
+	compressedData.resize(compressedDataSize);
+
+	std::vector<char> uncompressedData;
+	uncompressedData.resize(compressedData.size() * 4);
+	unsigned int uncompressedDataSize = uncompressedData.size();
+	CPPUNIT_ASSERT_EQUAL(BZ_OK, BZ2_bzBuffToBuffDecompress(uncompressedData.data(), &uncompressedDataSize, (char *)compressedData.data(), compressedData.size(), 0, 0));
+	uncompressedData.resize(uncompressedDataSize);
+
+	CPPUNIT_ASSERT_EQUAL(uncompressedData.size(), (std::vector<char>::size_type)objOrginal.objsize());
+
+	for (unsigned int i = 0; i < uncompressedData.size(); i++)
+		CPPUNIT_ASSERT_EQUAL(uncompressedData.operator [](i), objOrginal.objdata()[i]);
+
+	bson::bo objUnpacked(uncompressedData.data());
+
+	CPPUNIT_ASSERT_EQUAL(std::string("joe"), objUnpacked.getField("name").String());
+	CPPUNIT_ASSERT_EQUAL(33.7, objUnpacked.getField("age").Number());
 
 }
