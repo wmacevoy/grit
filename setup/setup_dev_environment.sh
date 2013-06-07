577127a9fe1a295e4e922c751b33e82eaa3878ac
mike <mike@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-06-07 02:24:33 +0000

    (Setup) Changed the setup so that a specific version of boost is not required.  This caused a problem after upgrading to ubuntu 13.04
    Former-commit-id: 40d1190fdb199855d98a08d926ea15e741a748cf

--- setup/setup_dev_environment.sh
+++ setup/setup_dev_environment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost*1.48-dev 
+apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost*-dev 
