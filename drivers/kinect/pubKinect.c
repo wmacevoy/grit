c8f156efc5ccd1cb5dfdfab117476da1248eda74
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-06-14 01:16:45 +0000

    Added the publisher and subsciber for the kinect.  Still a work in progress
    Former-commit-id: 31213b6cc13d10f43d070fd9e753d00b9af08590

---
+++ drivers/kinect/pubKinect.c
@@ -0,0 +1,318 @@
+/*
+ * This is the publisher for the kinect.
+ * Modified by Karl Castleton & Michaela Ervin from the glview example from libfreenect
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <zmq.h>
+#include <assert.h>
+
+#include "libfreenect.h"
+
+#include <pthread.h>
+
+#include <math.h>
+
+#define LONG  int
+#define DWORD unsigned int
+#define WORD unsigned short
+
+pthread_t freenect_thread;
+volatile int die = 0;
+
+// back: owned by libfreenect (implicit for depth)
+// mid: owned by callbacks, "latest frame ready"
+uint8_t *depth_mid;
+uint8_t *rgb_back, *rgb_mid;
+
+freenect_context *f_ctx;
+freenect_device *f_dev;
+int freenect_angle = 0;
+int freenect_led;
+
+freenect_video_format requested_format = FREENECT_VIDEO_RGB;
+freenect_video_format current_format = FREENECT_VIDEO_RGB;
+
+pthread_mutex_t buf_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t frame_cond = PTHREAD_COND_INITIALIZER;
+
+const int sz_img = 640*480*3;
+
+uint16_t t_gamma[2048];
+
+typedef struct __attribute__((packed)) tagBITMAPFILEHEADER
+{
+ WORD bfType;
+ DWORD bfSize;
+ WORD bfReserved1;
+ WORD bfReserved2;
+ DWORD bfOffBits;
+} BITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER
+{
+ DWORD biSize;
+ LONG biWidth;
+ LONG biHeight;
+ WORD biPlanes;
+ WORD biBitCount;
+ DWORD biCompression;
+ DWORD biSizeImage;
+ LONG biXPelsPerMeter;
+ LONG biYPelsPerMeter;
+ DWORD biClrUsed;
+ DWORD biClrImportant;
+} BITMAPINFOHEADER;
+
+void publish(uint8_t *image, void *zmq_pub)
+{
+	zmq_msg_t msg;
+	int rc = zmq_msg_init_size(&msg, sz_img);
+	assert(rc == 0);
+	memcpy(zmq_msg_data(&msg), image, sz_img);
+	rc = zmq_msg_send(&msg, zmq_pub, 0);
+	zmq_msg_close(&msg);
+}
+
+//Used for debugging, will not be used int pub version
+void CaptureScreen(int Width,int Height,uint8_t *image,char *fname,int fcount)
+{
+	BITMAPFILEHEADER bf;
+	BITMAPINFOHEADER bi;
+	char filename[255];
+
+	sprintf(filename,"%s%d.bmp",fname,fcount);
+
+	FILE *file	 = fopen(filename, "wb");
+
+	if( image!=NULL )
+	{
+		if( file!=NULL ) 
+		{
+			memset( &bf, 0, sizeof( bf ) );
+			memset( &bi, 0, sizeof( bi ) );
+
+
+			bf.bfType	 = 'MB';
+			bf.bfSize	 = sizeof(bf)+sizeof(bi)+Width*Height*3;
+			bf.bfOffBits	 = sizeof(bf)+sizeof(bi);
+			bi.biSize	 = sizeof(bi);
+			bi.biWidth	 = Width;
+			bi.biHeight	 = Height;
+			bi.biPlanes	 = 1;
+			bi.biBitCount	 = 24;
+			bi.biSizeImage	 = Width*Height*3;
+
+			fwrite( &bf, sizeof(bf), 1, file );
+			fwrite( &bi, sizeof(bi), 1, file );
+			int i;
+			for (i=0;i<bi.biSizeImage;i+=3) {
+				uint8_t c = image[i];
+				image[i] = image[i+2];
+				image[i+2] = c;
+			}
+			fwrite( image, sizeof(unsigned char), Height*Width*3, file );
+
+			fclose( file );
+		}
+	}
+}
+
+void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp)
+{
+	int i;
+	uint16_t *depth = (uint16_t*)v_depth;
+
+	pthread_mutex_lock(&buf_mutex);
+	for (i=0; i<640*480; i++) {
+		int pval = t_gamma[depth[i]];
+		int lb = pval & 0xff;
+		switch (pval>>8) {
+			case 0:
+				depth_mid[3*i+0] = 255;
+				depth_mid[3*i+1] = 255-lb;
+				depth_mid[3*i+2] = 255-lb;
+				break;
+			case 1:
+				depth_mid[3*i+0] = 255;
+				depth_mid[3*i+1] = lb;
+				depth_mid[3*i+2] = 0;
+				break;
+			case 2:
+				depth_mid[3*i+0] = 255-lb;
+				depth_mid[3*i+1] = 255;
+				depth_mid[3*i+2] = 0;
+				break;
+			case 3:
+				depth_mid[3*i+0] = 0;
+				depth_mid[3*i+1] = 255;
+				depth_mid[3*i+2] = lb;
+				break;
+			case 4:
+				depth_mid[3*i+0] = 0;
+				depth_mid[3*i+1] = 255-lb;
+				depth_mid[3*i+2] = 255;
+				break;
+			case 5:
+				depth_mid[3*i+0] = 0;
+				depth_mid[3*i+1] = 0;
+				depth_mid[3*i+2] = 255-lb;
+				break;
+			default:
+				depth_mid[3*i+0] = 0;
+				depth_mid[3*i+1] = 0;
+				depth_mid[3*i+2] = 0;
+				break;
+		}
+	}
+
+	pthread_cond_signal(&frame_cond);
+	pthread_mutex_unlock(&buf_mutex);
+}
+
+void rgb_cb(freenect_device *dev, void *rgb, uint32_t timestamp)
+{
+	pthread_mutex_lock(&buf_mutex);
+
+	// swap buffers
+	assert (rgb_back == rgb);
+	rgb_back = rgb_mid;
+	freenect_set_video_buffer(dev, rgb_back);
+	rgb_mid = (uint8_t*)rgb;
+
+	pthread_cond_signal(&frame_cond);
+	pthread_mutex_unlock(&buf_mutex);
+}
+
+void *freenect_threadfunc(void *arg)
+{
+	freenect_set_led(f_dev,LED_GREEN);
+	freenect_set_depth_callback(f_dev, depth_cb);
+	freenect_set_video_callback(f_dev, rgb_cb);
+	freenect_set_video_mode(f_dev, freenect_find_video_mode(FREENECT_RESOLUTION_MEDIUM, current_format));
+	freenect_set_depth_mode(f_dev, freenect_find_depth_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_11BIT));
+	freenect_set_video_buffer(f_dev, rgb_back);
+
+	freenect_start_depth(f_dev);
+	freenect_start_video(f_dev);
+
+	while (!die && freenect_process_events(f_ctx) >= 0) {
+
+		if (requested_format != current_format) {
+			freenect_stop_video(f_dev);
+			freenect_set_video_mode(f_dev, freenect_find_video_mode(FREENECT_RESOLUTION_MEDIUM, requested_format));
+			freenect_start_video(f_dev);
+			current_format = requested_format;
+		}
+	}
+
+	printf("\nshutting down streams...\n");
+
+	freenect_stop_depth(f_dev);
+	freenect_stop_video(f_dev);
+
+	freenect_close_device(f_dev);
+	freenect_shutdown(f_ctx);
+
+	printf("-- done!\n");
+	return NULL;
+}
+
+int main(int argc, char** argv)
+{
+	int res;
+	int quit = 0;
+	int fcount = 0;
+
+	void *context_color = zmq_ctx_new ();
+	void *pub_color = zmq_socket(context_color, ZMQ_PUB);
+	int rcc = zmq_bind(pub_color, "tcp://*:5556");
+
+	void *context_depth = zmq_ctx_new ();
+	void *pub_depth = zmq_socket(context_depth, ZMQ_PUB);
+	int rcd = zmq_bind(pub_depth, "tcp://*:5557");	
+
+	depth_mid = (uint8_t*)malloc(640*480*3);
+	rgb_back = (uint8_t*)malloc(640*480*3);
+	rgb_mid = (uint8_t*)malloc(640*480*3);
+
+	//Initialize
+	int i;
+	for (i=0; i<2048; i++) {
+		float v = i/2048.0;
+		v = powf(v, 3)* 6;
+		t_gamma[i] = v*6*256;
+	}
+
+	if (freenect_init(&f_ctx, NULL) < 0) {
+		printf("freenect_init() failed\n");
+		return 1;
+	}
+
+	//freenect_set_log_level(f_ctx, FREENECT_LOG_DEBUG);
+	freenect_select_subdevices(f_ctx, (freenect_device_flags)(FREENECT_DEVICE_MOTOR | FREENECT_DEVICE_CAMERA));
+
+	int nr_devices = freenect_num_devices (f_ctx);
+	printf ("Number of devices found: %d\n", nr_devices);
+
+	int user_device_number = 0;
+	if (argc > 1) {
+		user_device_number = atoi(argv[1]);
+		printf("%s", argv[1]);
+	}
+
+	if (nr_devices < 1) {
+		freenect_shutdown(f_ctx);
+		return 1;
+	}
+
+	if (freenect_open_device(f_ctx, &f_dev, 0) < 0) {
+		printf("Could not open device\n");
+		freenect_shutdown(f_ctx);
+		return 1;
+	}
+
+	res = pthread_create(&freenect_thread, NULL, freenect_threadfunc, NULL);
+	if (res) {
+		printf("pthread_create failed\n");
+		freenect_shutdown(f_ctx);
+		return 1;
+	}
+
+	//Sleep for 1 second to allow thread to initialize
+	sleep(1);
+
+	while(!quit)
+	{
+		pthread_mutex_lock(&buf_mutex);
+
+		/*Capture images to file, comment out following block {} for release
+		{
+		CaptureScreen(640,480,rgb_mid,"color",fcount);
+		CaptureScreen(640,480,depth_mid,"depth",fcount);
+		fcount++;
+		}
+		//*/
+
+		//Send image data via zmq
+		publish(rgb_mid, pub_color);
+		publish(depth_mid, pub_depth);
+		
+		pthread_cond_signal(&frame_cond);
+		pthread_mutex_unlock(&buf_mutex);
+
+	}
+	
+	//Cleanup
+	zmq_ctx_destroy(context_color);
+	zmq_ctx_destroy(context_depth);
+
+	free(depth_mid);
+	free(rgb_back);
+	free(rgb_mid);
+
+	return 0;
+}
---
+++ drivers/kinect/subKinect.c
@@ -0,0 +1,298 @@
+/*
+ * This is the subscriber for the kinect.
+ * Written by Michaela Ervin & Karl Castleton and using modifications from the glview example from libfreenect
+ * 
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <assert.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <zmq.h>
+
+#define LONG  int
+#define DWORD unsigned int
+#define WORD unsigned short
+
+const int sz_img = 640*480*3;
+
+int saveImage = 0;
+
+int g_argc;
+char **g_argv;
+
+int window;
+
+void* sub_color;
+void* sub_depth;
+
+uint8_t* img_color;
+uint8_t* img_depth;
+
+GLuint gl_depth_tex;
+GLuint gl_rgb_tex;
+
+typedef struct __attribute__((packed)) tagBITMAPFILEHEADER
+{
+ WORD bfType;
+ DWORD bfSize;
+ WORD bfReserved1;
+ WORD bfReserved2;
+ DWORD bfOffBits;
+} BITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER
+{
+ DWORD biSize;
+ LONG biWidth;
+ LONG biHeight;
+ WORD biPlanes;
+ WORD biBitCount;
+ DWORD biCompression;
+ DWORD biSizeImage;
+ LONG biXPelsPerMeter;
+ LONG biYPelsPerMeter;
+ DWORD biClrUsed;
+ DWORD biClrImportant;
+} BITMAPINFOHEADER;
+
+void CaptureScreen(int Width,int Height,uint8_t *image,char *fname,int fcount);
+
+
+///////////////////////////////////////////////////////OpenGL START
+void DrawGLScene()
+{
+	subscribe_color(sub_color);
+	//subscribe_depth(sub_depth);	
+
+	glBindTexture(GL_TEXTURE_2D, gl_depth_tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, 640, 480, 0, GL_RGB, GL_UNSIGNED_BYTE, img_depth);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0, 0); glVertex3f(0,0,0);
+	glTexCoord2f(1, 0); glVertex3f(640,0,0);
+	glTexCoord2f(1, 1); glVertex3f(640,480,0);
+	glTexCoord2f(0, 1); glVertex3f(0,480,0);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, gl_rgb_tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, 640, 480, 0, GL_RGB, GL_UNSIGNED_BYTE, img_color);
+	glTexImage2D(GL_TEXTURE_2D, 0, 1, 640, 480, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, img_depth+640*4);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0, 0); glVertex3f(640,0,0);
+	glTexCoord2f(1, 0); glVertex3f(1280,0,0);
+	glTexCoord2f(1, 1); glVertex3f(1280,480,0);
+	glTexCoord2f(0, 1); glVertex3f(640,480,0);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+void keyPressed(unsigned char key, int x, int y)
+{
+
+	if (key == 'c'){
+		saveImage = 1;
+	}
+	if (key == 27) {
+		glutDestroyWindow(window);
+		// Not pthread_exit because OSX leaves a thread lying around and doesn't exit
+		exit(0);
+	}
+}
+
+void ReSizeGLScene(int Width, int Height)
+{
+	glViewport(0,0,Width,Height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho (0, 1280, 480, 0, -1.0f, 1.0f);
+	glMatrixMode(GL_MODELVIEW);
+    	glLoadIdentity();
+}
+
+void InitGL(int Width, int Height)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0);
+	glDepthFunc(GL_LESS);
+    	glDepthMask(GL_FALSE);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_BLEND);
+	glDisable(GL_ALPHA_TEST);
+	glEnable(GL_TEXTURE_2D);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glShadeModel(GL_FLAT);
+
+	glGenTextures(1, &gl_depth_tex);
+	glBindTexture(GL_TEXTURE_2D, gl_depth_tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glGenTextures(1, &gl_rgb_tex);
+	glBindTexture(GL_TEXTURE_2D, gl_rgb_tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	ReSizeGLScene(Width, Height);
+}
+
+void *gl_threadfunc(void *arg)
+{
+	printf("GL thread\n");
+
+	glutInit(&g_argc, g_argv);
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+	glutInitWindowSize(1280, 480);
+	glutInitWindowPosition(0, 0);
+
+	window = glutCreateWindow("LibFreenect");
+
+	glutDisplayFunc(&DrawGLScene);
+	glutIdleFunc(&DrawGLScene);
+	glutReshapeFunc(&ReSizeGLScene);
+	glutKeyboardFunc(&keyPressed);
+
+	InitGL(1280, 480);
+
+	glutMainLoop();
+
+	return NULL;
+}
+///////////////////////////////////////////////////////OpenGL END
+
+
+//Both subscribers need to be threaded
+void subscribe_color(void *zmq_sub) 
+{
+	static int fcount = 0;
+
+	printf("waiting...\n");
+
+	zmq_msg_t msg;
+	zmq_msg_init(&msg);
+	zmq_msg_recv(&msg, zmq_sub, 0);
+	memcpy(img_color, zmq_msg_data(&msg), sz_img);
+
+	printf("received!\n");
+	
+	if(img_color != NULL && saveImage)
+	{
+		CaptureScreen(640, 480, img_color, "color_", fcount);
+		fcount++; 
+		saveImage = 0;
+	}
+}
+
+void subscribe_depth(void *zmq_sub) 
+{
+	static int fcount = 0;
+
+	printf("waiting...\n");
+
+	zmq_msg_t msg;
+	zmq_msg_init(&msg);
+	zmq_msg_recv(&msg, zmq_sub, 0);
+	memcpy(img_depth, zmq_msg_data(&msg), sz_img);
+
+	printf("received!\n");
+	
+	if(img_depth != NULL && saveImage)
+	{
+		CaptureScreen(640, 480, img_depth, "depth_", fcount);
+		fcount++; 
+		saveImage = 0;
+	}
+}
+
+void CaptureScreen(int Width,int Height,uint8_t *image,char *fname,int fcount)
+{
+	BITMAPFILEHEADER bf;
+	BITMAPINFOHEADER bi;
+	char filename[255];
+
+	sprintf(filename,"%s%d.bmp",fname,fcount);
+
+	FILE *file	 = fopen(filename, "wb");
+
+	if( image!=NULL )
+	{
+		if( file!=NULL ) 
+		{
+			memset( &bf, 0, sizeof( bf ) );
+			memset( &bi, 0, sizeof( bi ) );
+
+
+			bf.bfType	 = 'MB';
+			bf.bfSize	 = sizeof(bf)+sizeof(bi)+Width*Height*3;
+			bf.bfOffBits	 = sizeof(bf)+sizeof(bi);
+			bi.biSize	 = sizeof(bi);
+			bi.biWidth	 = Width;
+			bi.biHeight	 = Height;
+			bi.biPlanes	 = 1;
+			bi.biBitCount	 = 24;
+			bi.biSizeImage	 = Width*Height*3;
+
+			fwrite( &bf, sizeof(bf), 1, file );
+			fwrite( &bi, sizeof(bi), 1, file );
+			int i;
+			for (i=0;i<bi.biSizeImage;i+=3) {
+				uint8_t c = image[i];
+				image[i] = image[i+2];
+				image[i+2] = c;
+			}
+			fwrite( image, sizeof(unsigned char), Height*Width*3, file );
+
+			fclose( file );
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	int quit = 0;
+
+	void *context_color = zmq_ctx_new ();
+	void *context_depth = zmq_ctx_new ();
+
+	sub_color = zmq_socket(context_color, ZMQ_SUB);
+	sub_depth = zmq_socket(context_depth, ZMQ_SUB);
+
+	if (zmq_connect(sub_color, "tcp://localhost:5556") !=0 || zmq_connect(sub_depth, "tcp://localhost:5557") !=0 )
+	{
+
+	}
+
+	int rcc = zmq_setsockopt (sub_color, ZMQ_SUBSCRIBE, "", 0);
+	int rcd = zmq_setsockopt (sub_depth, ZMQ_SUBSCRIBE, "", 0);
+	assert (rcc == 0 && rcd == 0);
+	
+	img_color = (uint8_t*)malloc(sz_img);
+	img_depth = (uint8_t*)malloc(sz_img);
+
+	gl_threadfunc(NULL);
+
+	/*//Main Loop
+	while(!quit)
+	{
+		subscribe(sub);
+		sleep(1);
+	}*/
+
+	zmq_close (sub_color);
+	zmq_ctx_destroy (context_color);
+
+	zmq_close (sub_depth);
+	zmq_ctx_destroy (context_depth);
+
+	return 0;
+}
+	
