6be5408641f732db98e602fd9e4e164fbd8eafff
karl <karl@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-06-23 22:09:31 +0000

    Updated to use new C++11 time to throttle speed of messages from the Orientation and Leg Pressure sensors.
    Former-commit-id: 8007b375c93f1b043367ae820ffdafed085780fb

--- centaur/centaur/PubOrientationLegPressure/src/PubOrientationLegPressure.cpp
+++ centaur/centaur/PubOrientationLegPressure/src/PubOrientationLegPressure.cpp
@@ -8,28 +8,41 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 #include "LegPressure.hpp"
 #include "Orientation.hpp"
 
-#define BAUDRATE B9600            
+#define BAUDRATE B9600
+
+            
+steady_clock::time_point last=steady_clock::now();
 
 void publish(Orientation *o,LegPressure *lp,void *zmq_pub) {
-      o->print(cout);
-      lp->print(cout);
-      zmq_msg_t msg;
-      int rc=zmq_msg_init_size(&msg,sizeof(Orientation));
-      assert(rc==0);
-      memcpy(zmq_msg_data(&msg),o,sizeof(Orientation));
-      rc=zmq_msg_send(&msg,zmq_pub,0);
-      zmq_msg_close(&msg);
-      rc=zmq_msg_init_size(&msg,sizeof(LegPressure));
-      assert(rc==0);
-      memcpy(zmq_msg_data(&msg),lp,sizeof(LegPressure));
-      rc=zmq_msg_send(&msg,zmq_pub,0);
-      zmq_msg_close(&msg);
+      steady_clock::time_point next=steady_clock::now();
+      int diff=duration_cast<milliseconds>(next-last).count();
+//      cout << diff << endl;
+      if (diff>100) { 
+//        o->print(cout);
+//        lp->print(cout);
+        cout << '.';
+        cout.flush();
+        zmq_msg_t msg;
+        int rc=zmq_msg_init_size(&msg,sizeof(Orientation));
+        assert(rc==0);
+        memcpy(zmq_msg_data(&msg),o,sizeof(Orientation));
+        rc=zmq_msg_send(&msg,zmq_pub,0);
+        zmq_msg_close(&msg);
+        rc=zmq_msg_init_size(&msg,sizeof(LegPressure));
+        assert(rc==0);
+        memcpy(zmq_msg_data(&msg),lp,sizeof(LegPressure));
+        rc=zmq_msg_send(&msg,zmq_pub,0);
+        zmq_msg_close(&msg);
+        last=next;
+      }
 }
 
 int main(int argc,char **argv) {
@@ -42,7 +55,7 @@ int main(int argc,char **argv) {
   int fd;
   ifstream in;
   if (argc!=2) {
-	  cerr << "Run PubOrientationLegPressure <serial> <ZMQ Com>"<<endl;
+	  cerr << "Run PubOrientationLegPressure <serial>"<<endl;
 	  cerr << "  <serial> Serial Port /dev/ttyUSB0 ..."<<endl;
 	  return -1;
   }
@@ -75,22 +88,25 @@ int main(int argc,char **argv) {
   while(true)
   {  
 	 char buff[1024];
-	 read(fd,buff, sizeof(buff));
+	 int n=read(fd,buff, sizeof(buff));
+	 buff[n-1]=0;
 	 istringstream ss(buff);
-	 cout <<"Line:{"<<buff<<"}"<<endl;
-	 string A,C,G,L;
-	 char c;
+//	 cout <<"Line:{"<<buff<<"}"<<endl;
+	 char A,C,G,L,c;
 	 int ax,ay,az,cx,cy,cz,gx,gy,gz,l1,l2,l3,l4;
 	 ss>> 
 	   A >> c >> ax >> c >> ay >> c >> az >> c >> // A,ax,ay,az,
 	   C >> c >> cx >> c >> cy >> c >> cz >> c >> // C,cx,cy,cz,
-	   G >> c >> gx >> c >> gy >> c >> gz >>  // G,gx,gy,gz,
+	   G >> c >> gx >> c >> gy >> c >> gz >> c >> // G,gx,gy,gz,
 	   L >> c >> l1 >> c >> l2 >> c>> l3 >> c >> l4;  // The L,l1,l2,l3,l4
+//	 cout << '.' <<endl;
 	 // Mapping to different types or to 
-	 // different values should occur here 
-     o.set(ax,ay,az,cx,cy,cz,gx,gy,gz);
-     lp.set(l1,l2,l3,l4);
-     publish(&o,&lp,pub);
+	 // different values should occur here
+	 if (A=='A' && C=='C' && G=='G' && L=='L') { 
+       o.set(ax,ay,az,cx,cy,cz,gx,gy,gz);
+       lp.set(l1,l2,l3,l4);
+       publish(&o,&lp,pub);
+     }
   }
   zmq_ctx_destroy(context);
   return 0;
