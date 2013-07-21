5431e9bb4c010f1702e531ba9bd7258056607fd9
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-07-21 07:36:40 +0000

    cleanup
    Former-commit-id: 180dc7e24bfb7acabacf1183b85c76bb807713a5

--- drivers/utilities/src/test_csvread.cpp
+++ drivers/utilities/src/test_csvread.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <assert.h>
 
 #include "CSVRead.h"
 
@@ -14,12 +15,13 @@ int main()
 
   string heading="a,b,c";
   CSVRead("example.csv","a,b,c",table);
-
-  cout << "a,b,c" << endl;
+  assert(table.size() == 2);
   for (size_t i=0; i<table.size(); ++i) {
+    assert(table[i].size() == 3);
     for (size_t j=0; j<table[i].size(); ++j) {
-      cout << ((j > 0) ? "," : "") << table[i][j];
+      assert(table[i][j]==10*i+j);
     }
-    cout << endl;
   }
+  cout << "ok" << endl;
+  return 0;
 }
