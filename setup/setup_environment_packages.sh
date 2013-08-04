631467e645e74e54a2f32f3bdefd9bacf24440b5
mike <mike@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-08-04 19:37:10 +0000

    (Setup) Added some additional packages that are needed to build the freenect library.
    Former-commit-id: 9b922f6adfab8c8814e2a63b40a0872ddfbbdf56

--- setup/setup_environment_packages.sh
+++ setup/setup_environment_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt-get install realpath freeglut3-dev libsdl-dev libsdl-net1.2-dev libfreeimage-dev realpath
+apt-get install realpath freeglut3-dev libsdl-dev libsdl-net1.2-dev libfreeimage-dev realpath libxmu-dev libxi-dev 
 
 
