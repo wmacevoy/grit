224a2ca71b5037962c422a865552ec5009fbea9e
karl <karl@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-05 03:45:47 +0000

    -m build! - in control
    Former-commit-id: 4afd94e1394ae94551bac7eb1896daa59fe7c2cb

--- control/config.mk
+++ control/config.mk
@@ -1,5 +1,5 @@
-CONTROL_FLAGS=-Iinclude -I../../libs/include -I../../setup
-CONTROL_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread -Wl,--no-as-needed -ldl -lrt
+CONTROL_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson
+CONTROL_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread-mt -lboost_date_time-mt -ljson -Wl,--no-as-needed -ldl -lrt
 
 CFLAGS += $(CONTROL_FLAGS)
 CXXFLAGS += $(CONTROL_FLAGS)
