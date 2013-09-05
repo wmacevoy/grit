ff116ea0e394782822e9f8e3ee11f6c5eaba8ea1
karl <karl@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-05 03:45:45 +0000

    -m build! - in setup
    Former-commit-id: 94ac5bb1812561658009b81a32b6eed8f1c564c0

--- setup/setup_dev_environment.sh
+++ setup/setup_dev_environment.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost*-dev cmake
-
+apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost*-dev cmake libusb-1.0-0-dev
