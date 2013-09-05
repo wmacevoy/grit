1684b48cfe7cb5f248fc0f77921da7d17e3f6d2b
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-05 02:33:18 +0000

    -m build - in centaur
    Former-commit-id: 17a1b3f13072fdebfa209b2310426739aa6fd9f5

--- centaur/config.mk
+++ centaur/config.mk
@@ -1,5 +1,5 @@
 CENTAUR_FLAGS=-Iinclude -I../../libs/include -I../../setup
-CENTAUR_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -lcppunit2 -ldxl -lfreenect -lfreenect_sync -lhokuyo -lzmq -lSDL -lpthread -lboost_thread -Wl,--no-as-needed -ldl -lrt
+CENTAUR_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread -Wl,--no-as-needed -ldl -lrt
 
 CFLAGS += $(CENTAUR_FLAGS)
 CXXFLAGS += $(CENTAUR_FLAGS)
