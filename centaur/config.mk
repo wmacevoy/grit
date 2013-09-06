15115ff7eede318a5952b7594c6cc2dc454d9618
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-06 03:41:09 +0000

    fixy
    Former-commit-id: 5f3fa55d649a2812415a3245da30a31d579df64a

--- centaur/config.mk
+++ centaur/config.mk
@@ -1,4 +1,4 @@
-CENTAUR_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson -I../../libs/hokuyo/include
+CENTAUR_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson -I../../libs/hokuyo/include/c
 CENTAUR_LIBS=-L../../libs/lib -L../../libs/libjson -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread -ljson -Wl,--no-as-needed -ldl -lrt
 
 CFLAGS += $(CENTAUR_FLAGS)
