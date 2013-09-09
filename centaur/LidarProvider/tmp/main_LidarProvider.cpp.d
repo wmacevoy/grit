72e7da94094d8bb2cf2b9deed77c6da2ef76210f
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-09 12:53:18 +0000

    removed tmp file that was accidently added
    Former-commit-id: 3cc4015cb24b2ad704ff2a932cab2a9bbf04c83b

--- centaur/LidarProvider/tmp/main_LidarProvider.cpp.d
+++
@@ -1,7 +0,0 @@
-tmp/main_LidarProvider.cpp.o: src/main_LidarProvider.cpp \
- ../../libs/include/zmq.h /usr/local/include/c_urg/urg_ctrl.h \
- /usr/local/include/c_urg/urg_t.h \
- /usr/local/include/c_urg/urg_parameter_t.h \
- /usr/local/include/c_urg/serial_t.h /usr/local/include/c_urg/detect_os.h \
- /usr/local/include/c_urg/serial_t_lin.h \
- /usr/local/include/c_urg/ring_buffer.h
