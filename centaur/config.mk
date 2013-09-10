e84e649e50714f0ea5f7797e4b20257e5ad14cc3
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-10 19:47:09 +0000

    HokuyoProvider update and hokuyo libs build
    Former-commit-id: f601927af58c5d14eebcdb75dc879124a0800cb6

--- centaur/config.mk
+++ centaur/config.mk
@@ -1,5 +1,5 @@
-CENTAUR_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson `c_urg-config --cflags`
-CENTAUR_LIBS=-L../../libs/lib -L../../libs/libjson -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread -ljson -Wl,--no-as-needed -ldl -lrt `c_urg-config --libs` -lm
+CENTAUR_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson `../../libs/bin/c_urg-config --cflags`
+CENTAUR_LIBS=-L../../libs/lib -L../../libs/libjson -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread -ljson -Wl,--no-as-needed -ldl -lrt `../../libs/bin/c_urg-config --libs` -lm
 
 CFLAGS += $(CENTAUR_FLAGS)
 CXXFLAGS += $(CENTAUR_FLAGS)
--- control/config.mk
+++ control/config.mk
@@ -1,5 +1,5 @@
-CONTROL_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson `c_urg-config --cflags`
-CONTROL_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread-mt -lboost_date_time-mt -ljson -Wl,--no-as-needed -ldl -lrt `c_urg-config --libs` -lm
+CONTROL_FLAGS=-Iinclude -I../../libs/include -I../../setup -I../../libs/libjson `../../libs/bin/c_urg-config --cflags`
+CONTROL_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -lpthread -lboost_thread-mt -lboost_date_time-mt -ljson -Wl,--no-as-needed -ldl -lrt `../../libs/bin/c_urg-config --libs` -lm
 
 CFLAGS += $(CONTROL_FLAGS)
 CXXFLAGS += $(CONTROL_FLAGS)
