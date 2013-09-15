8bf41ff470ae2647ecfe71fbe07caacbe0b21589
sam <sam@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-15 22:01:19 +0000

    added swig to setup_dev_environment.sh
    Former-commit-id: 304b01f2e8ddc7f341a2aa3e8751ac559e4e3587

--- setup/setup_dev_environment.sh
+++ setup/setup_dev_environment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost-all-dev cmake libusb-1.0-0-dev
+apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost-all-dev cmake libusb-1.0-0-dev swig
