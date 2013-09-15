bcb8ea07521f06256fc0ecb438af23001aa75d6e
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-15 19:22:08 +0000

    Added webcamRequester
    Former-commit-id: cd6026fa949eab221752f36e6d36a1dcff36387a

---
+++ control/webcamRequester/src/main_webcamRequester.cpp
@@ -0,0 +1,78 @@
+#include "opencv2/opencv.hpp"
+#include "cv.h"
+#include "highgui.h"
+#include <iostream>
+#include <signal.h>
+#include <assert.h>
+#include <zmq.h>
+
+//#include "Configure.h"
+
+using namespace cv;
+
+bool die = false;
+bool verbose = false;
+
+const int sz_mat = 640*480*1;
+
+void subscribe(Mat& mat, void* zmq_sub)
+{
+	int rc = zmq_recv(zmq_sub, mat.data, sz_mat, ZMQ_DONTWAIT);
+	if(verbose) std::cout << "Received: " << rc << std::endl;
+}
+
+void quitproc(int param)
+{
+	std::cout << "Quitting..." << std::endl;
+	die = true;
+}
+
+int main(int argc, char** argv)
+{
+	//cfg.path("../../setup");
+	//cfg.args("webcam.provider.", argv);
+	//if (argc == 1) cfg.load("config.csv");
+	//verbose = cfg.flag("webcam.provider.verbose", false);
+	//if (verbose) cfg.show();
+	
+	int hwm = 1;
+	int rc = 0;
+	Mat gray(480, 640, 0);
+	std::string winName = "ICU";
+	namedWindow(winName, WINDOW_KEEPRATIO);
+
+	void* context_mat = zmq_ctx_new ();
+	
+	void* sub_mat = zmq_socket(context_mat, ZMQ_PUB);
+	rc = zmq_setsockopt(sub_mat, ZMQ_SNDHWM, &hwm, sizeof(hwm));
+	assert(rc == 0);
+
+	sub_mat = zmq_socket(context_mat, ZMQ_SUB);
+
+	rc = zmq_setsockopt(sub_mat, ZMQ_RCVHWM, &hwm, sizeof(hwm));
+	assert (rc == 0);
+
+	rc = zmq_setsockopt(sub_mat, ZMQ_SUBSCRIBE, "", 0);
+	assert (rc == 0);
+
+	rc = zmq_connect(sub_mat, "tcp://localhost:9993");
+	assert(rc == 0);	
+
+	signal(SIGINT, quitproc);
+	signal(SIGQUIT, quitproc);
+
+	std::cout << gray.total() << std::endl;
+
+	while(!die)
+	{
+		subscribe(gray, sub_mat);
+		imshow(winName, gray);
+		char c = waitKey(100);
+		if(c == 'q') die = 1;
+	}
+
+	destroyWindow(winName);
+	zmq_close(sub_mat);
+	zmq_ctx_destroy(context_mat);
+	return 0;
+}
