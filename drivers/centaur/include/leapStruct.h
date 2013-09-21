654b889564d7a0e38666de5827cbb569e1f3b7f8
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-21 19:46:08 +0000

    added leapStruct.h
    Former-commit-id: 2cebb981748af89e7e417de73c3429d7c79ac191

---
+++ drivers/centaur/include/leapStruct.h
@@ -0,0 +1,27 @@
+struct leapData{
+	float x;
+	float y;
+	float z;
+	float normala;
+	float normalb;
+	float normalc;
+
+	leapData()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		normala = 0;
+		normalb = 0;
+		normalc = 0;
+	}
+	void clear()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		normala = 0;
+		normalb = 0;
+		normalc = 0;
+	}
+};
