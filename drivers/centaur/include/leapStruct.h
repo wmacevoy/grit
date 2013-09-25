088a687305d303c424c83aacc2c7985e31c4598d
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-25 21:13:53 +0000

    updated leapStruct.h
    Former-commit-id: c3016a90d1e776d999d7a146ef7880f3446d2a57

--- drivers/centaur/include/leapStruct.h
+++ drivers/centaur/include/leapStruct.h
@@ -1,15 +1,25 @@
 struct leapData{
-	float x;
-	float y;
-	float z;
-	float roll;
+	float lx;
+	float ly;
+	float lz;
+	float lroll;
+
+	float rx;
+	float ry;
+	float rz;
+	float rroll;
 
 	leapData() {clear();}
 	void clear()
 	{
-		x = 0;
-		y = 0;
-		z = 0;
-		roll = 0;
+		lx = 0;
+		ly = 0;
+		lz = 0;
+		lroll = 0;
+
+		rx = 0;
+		ry = 0;
+		rz = 0;
+		rroll = 0;
 	}
 };
