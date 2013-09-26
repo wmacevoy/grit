bc6c11c6ae3005ad94de8fab486de17f2d54e128
karl <karl@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-09-26 00:17:52 +0000

    added pitch
    Former-commit-id: 22ce2e6494e537dd7645fb3f5b28639454763bfc

--- drivers/centaur/include/leapStruct.h
+++ drivers/centaur/include/leapStruct.h
@@ -3,11 +3,13 @@ struct leapData{
 	float ly;
 	float lz;
 	float lroll;
+	float lpitch;
 
 	float rx;
 	float ry;
 	float rz;
 	float rroll;
+	float rpitch;
 
 	leapData() {clear();}
 	void clear()
@@ -16,10 +18,12 @@ struct leapData{
 		ly = 0;
 		lz = 0;
 		lroll = 0;
+		lpitch = 0;
 
 		rx = 0;
 		ry = 0;
 		rz = 0;
 		rroll = 0;
+		rpitch = 0;
 	}
 };
