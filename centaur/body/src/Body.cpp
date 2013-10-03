103c9e027d4f348a26a9eb2049b591567b8c744f
sam <sam@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-10-03 21:38:53 +0000

    temps
    Former-commit-id: 94a44afada53fec90451f0df11a44fd766c69f96

--- centaur/body/src/Body.cpp
+++ centaur/body/src/Body.cpp
@@ -32,7 +32,12 @@ void Body::temp_report(std::ostream &out, std::string part) const
     neck.upDown->temp_report(out);
     neck.leftRight->temp_report(out);
     }
-  /*else if (part == "legs")
-  legs.temp_report(out);*/
+  else if (part == "legs")
+    for (int i = 0; i < 4; i++)
+      {
+	legs[i].knee->temp_report(out);
+	legs[i].femur->temp_report(out);
+	legs[i].hip->temp_report(out);
+      }
   out << endl;
 }
