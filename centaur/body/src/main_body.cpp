b8517421f0d58d774ca3d3c280d1a99a49a35447
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-10-03 03:02:09 +0000

    fixed main body
    Former-commit-id: 34bf61ec4cfeabbd9d52a0cc76c550bff84d2c38

--- centaur/body/src/main_body.cpp
+++ centaur/body/src/main_body.cpp
@@ -109,7 +109,6 @@ public:
   void subscribeToNeck() {	//Neck thread function
 	float currentUpDown = 0;
 	float currentLeftRight = 0;
-	int currentLeftRight = 0;
 	int rc;
 	joystick jm;
 	std::string address = cfg->str("body.commander.neckAddress", "tcp://192.168.2.113:5556");
@@ -132,7 +131,6 @@ public:
 		subscribeN(sub, &jm);
 		currentUpDown = currentUpDown + (deltat)*(float(jm.y2)/32.0)*(15.0);
 		currentLeftRight = currentLeftRight + (deltat)*(float(jm.x2)/32.0)*(15.0);
-		currentUpDown = currentLeftRight + (deltat)*(jm.x2*jm.x2*jm.x2)/(32*32*32)*(15.0);
 		mover->neck.upDown.setup(currentUpDown);
 		mover->neck.leftRight.setup(currentLeftRight);
 		std::this_thread::sleep_for(std::chrono::microseconds(25));
