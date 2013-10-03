c57dc08d8dce17eb630b41b9a1b2e3b63b005c31
sam <sam@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-10-03 01:29:58 +0000

    joystick class added
    Former-commit-id: 00784c34987e8a24b02c4faa0af8346b0e5086fa

---
+++ drivers/centaur/include/joystick.h
@@ -0,0 +1,59 @@
+class joystick
+{
+public:
+	int32_t x1, y1, x2, y2;
+	bool button1, button2;
+	joystick()
+	{
+		x1 = 0;
+		y1 = 0;
+		x2 = 0;
+		y2 = 0;
+		button1 = false;
+		button2 = false;
+	}
+	void setX1(int32_t newX)
+	{
+		//MAP to max and min neck
+		x1 = newX;
+	}
+	void setY1(int32_t newY)
+	{
+		//MAP to max and min neck
+		y1 = newY;
+	}
+	void setX2(int32_t newX)
+	{
+		//MAP to max and min neck
+		x2 = newX;
+	}
+	void setY2(int32_t newY)
+	{
+		//MAP to max and min neck
+		y2 = newY;
+	}
+	void setButtonDown(int button)
+	{
+		switch(button)
+		{
+		case 1:
+			button1 = true;
+			break;
+		case 2:
+			button2 = true;
+			break;
+		}
+	}
+	void setButtonUp(int button)
+	{
+		switch(button)
+		{
+		case 1:
+			button1 = false;
+			break;
+		case 2:
+			button2 = false;
+			break;
+		}
+	}
+};
