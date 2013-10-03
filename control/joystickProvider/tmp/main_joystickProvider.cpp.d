98c1a33f0ab17a49c4411867df43dc57f3c5dfda
sam <sam@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-10-03 01:29:16 +0000

    removed extraneous files
    Former-commit-id: 246a301f910c635e039393740216e8777f290019

--- control/joystickProvider/tmp/main_joystickProvider.cpp.d
+++
@@ -1,3 +0,0 @@
-tmp/main_joystickProvider.cpp.o: src/main_joystickProvider.cpp \
- ../../libs/include/zmq.h ../../drivers/centaur/include/joystick.h \
- ../../drivers/utilities/include/Configure.h
--- control/joystickProvider/tmp/test_joystickRequester.cpp.d
+++
@@ -1,3 +0,0 @@
-tmp/test_joystickRequester.cpp.o: src/test_joystickRequester.cpp \
- ../../libs/include/zmq.h ../../drivers/centaur/include/joystick.h \
- ../../drivers/utilities/include/Configure.h
