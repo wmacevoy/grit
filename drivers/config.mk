e3aa9c88cb2af047ac13ecd142935c5cfff7da62
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-10-04 06:07:46 +0000

    move drivers/centaur to attic
    Former-commit-id: 6365487db1b7f6aeb3546e51a85c3373d6ba9877

--- drivers/config.mk
+++ drivers/config.mk
@@ -1,5 +1,6 @@
 DRIVER_FLAGS=-Iinclude -I../../libs/include -I../../setup
-DRIVER_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -ldl -lpthread -lboost_thread-mt -lboost_date_time-mt -Wl,--no-as-needed -lrt
+#DRIVER_LIBS=-L../../libs/lib -lartemis -lbson -lbz2 -ldxl -lfreenect -lfreenect_sync -lcppunit2 -lzmq -lSDL -ldl -lpthread -lboost_thread-mt -lboost_date_time-mt -Wl,--no-as-needed -lrt
+DRIVER_LIBS=-L../../libs/lib -ldxl -lcppunit2 -lzmq -ldl -lpthread -lboost_thread-mt -lboost_date_time-mt -Wl,--no-as-needed -lrt
 
 CFLAGS += $(DRIVER_FLAGS)
 CXXFLAGS += $(DRIVER_FLAGS)
