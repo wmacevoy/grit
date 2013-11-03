a8ab1e30c01a7d5c239b6487879c33a5ac5e4f08
sam <sam@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-11-03 21:25:19 +0000

    Got the temperature and pressure information
    Former-commit-id: 83cd713de2867bf91507493ff37781be699263f4

--- control/gui/src/window_test.py
+++ control/gui/src/window_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env/python
 
-import pyzeromqSub
+import ctypes
+from ctypes import c_uint32, c_uint8, c_int32
 import sys
-import re
 import pygtk, gtk
 import gobject
-#import zmq
+import zmq
 import time
-from random import randint
+import struct
 pygtk.require('2.0')
 
 textFile = "/home/mojavaton/Documents/src/python/readTest.txt"
@@ -15,7 +15,6 @@ servoTemp = "/home/mojavaton/trunk/servos.log"
 tempFile = "/home/mojavaton/Documents/src/python/servoTemp.txt"
 redLight = "/home/mojavaton/Downloads/icon_red_light.png"
 greenLight = "/home/mojavaton/Downloads/icon_green_light.png"
-array =  { "servos[11]":54, "servos[12]":87, "servos[13]":64, "servos[14]":75, "servos[15]":100, "servos[16]":120, "servos[17]":98, "servos[18]":80, "servos[19]":100 }
 
 class Base:
     def __init__(self):
@@ -25,16 +24,36 @@ class Base:
         self.redIcon.set_from_file(redLight)
         self.redIcon.hide()
 
-    def requestZMQ():
-        pyzeromqSub.doStuff()
+    def requestZMQ(self,widget):
+        context = zmq.Context()
+        port = '9001'
+
+        req = context.socket(zmq.REQ)
+        req.connect("tcp://192.168.2.101: %s" % (port))
         
-    #read in a text file and output
-    def readFile(self,widget):
-        inFile = open(textFile)
-        text = inFile.read()
-        print text
-        self.label2.set_label(str(text))
-        inFile.close()
+        for i in range(0,2):
+            message = ctypes.c_uint32(1)
+            req.send(message)
+            receive_msg = req.recv()
+            print map(ord,receive_msg)
+            
+    def getInt(x):
+        return x
+
+    def sensor(self,widget):
+        context = zmq.Context()
+        port = '5506'
+        
+        sub = context.socket(zmq.SUB)
+        sub.setsockopt(zmq.SUBSCRIBE, '')
+        sub.connect("tcp://192.168.2.101: %s" % (port))
+
+        for i in range(0,1):
+            print "receiving msg..\n"
+            sensors = sub.recv()
+            for j in range(0,56,4):
+                newSensors = struct.unpack("<l",sensors[j:j+4])
+                print newSensors
 
     #kills the process when window is closed
     def destroy(self,widget):
@@ -42,16 +61,9 @@ class Base:
         gtk.main_quit()
 
     def temp_check(self,widget):
-        #pyzeromqSub.request()
         new_array = pyzeromqSub.doStuff(array)
-        #a = [59, 40, 65, 100, 200, 120, 87, 54, 110]
-        #randomNum = randint(0,len(a)-1)
-        #num = a[randomNum]
         for i in new_array:
-            #printTemp = "Temperature: %d* " % (num)
             print i
-            #self.label3.set_text(pyzeromqSub.request())
-            #print printTemp
             if num >= 80:
                 self.redIcon.show()
                 self.greenIcon.hide()
@@ -63,20 +75,18 @@ class Base:
     def win(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("destroy", lambda q: gtk.main_quit())
-        self.window.resize(600,700)
+        self.window.resize(150,730)
         self.window.set_title("Mojavaton Project")
         self.window.set_position(gtk.WIN_POS_CENTER)
         
         #buttons 1-4
         self.button1 = gtk.Button("Close")
         self.button1.connect("clicked",self.destroy)
-        self.button2 = gtk.Button("Get file")
-        self.button2.connect("clicked",self.readFile)
+        self.button2 = gtk.Button("Sensor")
+        self.button2.connect("clicked",self.sensor)
         self.button3 = gtk.Button("Temperature")
-        self.button3.connect("clicked",self.temp_check)
-        self.button4 = gtk.Button("Send info")
-        #self.button4.connect("clicked",self.subscriber)
-
+        self.button3.connect("clicked",self.requestZMQ)
+        
         #labels 1-4
         self.label1 = gtk.Label(" ")
         self.label2 = gtk.Label("This is where the temp goes")
@@ -90,10 +100,9 @@ class Base:
         #self.greenIcon.set_from_file('/home/mojavaton/Downloads/icon_green_light.png')
 
         fixed = gtk.Fixed()
-        fixed.put(self.button1,273,10)
-        fixed.put(self.button2,5,10)
-        fixed.put(self.button3,70,10)
-        fixed.put(self.button4,173,10)
+        fixed.put(self.button1, 123,10)
+        fixed.put(self.button2, 300,10)
+        fixed.put(self.button3, 10,10)
         fixed.put(self.label2, 10, 40)
         fixed.put(self.label3, 10, 80)
         fixed.put(self.greenIcon, 400, 60)
