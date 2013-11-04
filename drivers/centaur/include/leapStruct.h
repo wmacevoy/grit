2f8bd7507d91d9b255a6ec25b1e059d7251172f1
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-11-04 21:16:18 +0000

    added palm normal
    Former-commit-id: 9ba5ea0e04bf66e0221b4fa260a5577da778672d

--- drivers/centaur/include/leapStruct.h
+++ drivers/centaur/include/leapStruct.h
@@ -5,13 +5,19 @@ struct leapData{
 	float lroll;
 	float lpitch;
         float lyaw;
+	float lnormal1;
+	float lnormal2;
+	float lnormal3;
 
 	float rx;
 	float ry;
 	float rz;
 	float rroll;
 	float rpitch;
-  //float ryaw;
+	float ryaw;
+	float rnormal1;
+	float rnormal2;
+	float rnormal3;
 
 	leapData() {clear();}
 	void clear()
@@ -21,11 +27,13 @@ struct leapData{
 		lz = 0;
 		lroll = 0;
 		lpitch = 0;
+		lyaw = 0;
 
 		rx = 0;
 		ry = 0;
 		rz = 0;
 		rroll = 0;
 		rpitch = 0;
+		ryaw = 0;
 	}
 };
