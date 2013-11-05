16dde8fc58327e265b6c7a23b6bc57f97943a087
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-11-05 19:42:28 +0000

    smoothed sawtooth
    Former-commit-id: 1747c9407813cc408d8e76e0d169b1a516a5ca04

---
+++ drivers/utilities/src/test_sawtooth.cpp
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <iostream>
+#include <fstream>
+
+#include "Sawtooth.h"
+
+using namespace std;
+
+int main()
+{
+  // length of full sawtooth step
+  double L = 2*M_PI;
+
+  // width of mollifying  (<= L)
+  double w = L/4;
+
+  Sawtooth s(L,w);
+
+  int n=100;
+
+  ofstream out("sawtooth.csv");
+  for (int i=-n; i<=2*n; ++i) {
+    double x=L*double(i)/double(n);
+    out << x << "," << s(x) << endl;
+  }
+}
