693390c468fc679f37e2d263737e1269f2316bed
sam <sam@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-11-07 02:11:24 +0000

    getting gui going
    Former-commit-id: 9a18fd5eb8a87d64850354e4b3a32fb6ccf9ef68

--- control/gui/src/window_test.py
+++ control/gui/src/window_test.py
@@ -8,6 +8,9 @@ import gobject
 import zmq
 import time
 import struct
+
+import threading
+import time
 pygtk.require('2.0')
 
 textFile = "/home/mojavaton/Documents/src/python/readTest.txt"
@@ -34,7 +37,7 @@ class Base:
         return temp
 
     #Subscribe sensor from ZMQ_PUB
-    def sensor_SUB():
+    def sensor_SUB(self):
         context = zmq.Context()
         port = '5506'
         
@@ -42,6 +45,7 @@ class Base:
         sub.setsockopt(zmq.SUBSCRIBE, '')
         sub.connect("tcp://192.168.2.101: %s" % (port))
 
+        temp = []
         for i in range(0,1):
             print "Receiving msg..\n"
             sensors = sub.recv()
@@ -59,9 +63,87 @@ class Base:
         print "Window terminated"
         gtk.main_quit()
 
+    def press_check1(self):
+        leg_dict = {}
+        pressures = []
+
+        temps = self.sensor_SUB()
+        print temps
+        for i in range(1,5):
+            leg_dict[i] = temps[-i]
+            pressures.append(i)
+        print leg_dict
+        for pressure in pressures:
+            sev = 0
+            if int(leg_dict[pressure]) < 800:
+                sev = 0
+            elif int(leg_dict[pressure]) < 900:
+                sev = 1
+            elif int(leg_dict[pressure]) < 1024:
+                sev = 2
+            else:
+                sev = 3
+            try:
+                self.pressure_btn[int(pressure)].set_color(gtk.gdk.color_parse(self.colors[sev]))
+            except:
+                print "not defined %d" % int(pressure)
+            else:
+                print "defined %d" % int(pressure)
+
+    def temp_check1(self):
+        servos_dict = {}
+        servos = []
+
+        temps = self.temp_REQ()
+        for item in temps:
+            for (i, x) in enumerate(item):
+                if i%2==0:
+                    servos_dict[x] = item[i+1]
+                    servos.append(x)
+        print servos_dict
+        for servo in servos:
+            sev = 0
+            if int(servos_dict[servo]) > 65:
+                sev = 0
+            elif int(servos_dict[servo]) > 45:
+                sev = 1
+            elif int(servos_dict[servo]) > 0:
+                sev = 2
+            else:
+                sev = 3
+            try:
+                self.color_btn[int(servo)].set_color(gtk.gdk.color_parse(self.colors[sev]))
+            except:
+                print "not defined %d" % int(servo)
+            else:
+                print "defined %d" % int(servo)
+
     def press_check(self,widget):
         leg_dict = {}
         pressures = []
+
+        temps = self.sensor_SUB()
+        print temps
+        for i in range(1,5):
+            leg_dict[i] = temps[-i]
+            pressures.append(i)
+        print leg_dict
+        for pressure in pressures:
+            sev = 0
+            if int(leg_dict[pressure]) < 800:
+                sev = 0
+            elif int(leg_dict[pressure]) < 900:
+                sev = 1
+            elif int(leg_dict[pressure]) < 1024:
+                sev = 2
+            else:
+                sev = 3
+            try:
+                self.pressure_btn[int(pressure)].set_color(gtk.gdk.color_parse(self.colors[sev]))
+            except:
+                print "not defined %d" % int(pressure)
+            else:
+                print "defined %d" % int(pressure)
         
     def temp_check(self,widget):
         servos_dict = {}
@@ -95,8 +177,23 @@ class Base:
         self.focused=widget
         #print(widget.get_name())
 
+    class Update(threading.Thread):
+        def __init__(self, base_, delay_=2):
+            threading.Thread.__init__(self)
+            self.base = base_
+            self.delay = delay_
+        def run(self):
+            i = 0
+            while True:
+                i += 1
+                print "Update %d" % i
+                self.base.press_check1()
+                self.base.temp_check1()
+                time.sleep(self.delay)
+
     def quit(self,widget):
         print("QUIT")
+        self.update.join()
         gtk.main_quit()
 
     def __init__(self):
@@ -130,8 +227,17 @@ class Base:
       #  self.data.connect("focus-in-event", self.focus_received)
         #self.entry2.connect("focus-in-event", self.focus_received)
         self.focused=self.btnTemps
+        self.update = self.Update(self,2)
+        self.update.start()
 
 if __name__ == "__main__":
     base = Base()
     base.window.show()
+    gobject.threads_init()
+    gtk.gdk.threads_init()
+    gtk.gdk.threads_enter()
     gtk.main()
+    #update.join()
+    gtk.gdk.threads_leave()
+    #update.join()
+    
