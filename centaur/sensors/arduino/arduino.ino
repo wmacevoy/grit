1317ca5d388064d3db8486fad4082097ac23123a
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-11-19 16:59:50 +0000

    add arduino
    Former-commit-id: d8382de8d4009ca5157f7310f4b74dffafc9c938

---
+++ centaur/sensors/arduino/arduino.ino
@@ -0,0 +1,325 @@
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+#define GYROADDR 0x68
+#define COMPASSADDR 0x1e
+#define ACCELADDR 0x53
+
+union XYZBuffer {
+  struct {
+    short x,y,z;
+  } value;
+  byte buff[6];
+};
+
+void changeEndian(union XYZBuffer *xyz) {
+  for (int i=0;i<6;i+=2) {
+    byte t=xyz->buff[i];
+    xyz->buff[i]=xyz->buff[i+1];
+    xyz->buff[i+1]=t;
+  }
+}
+
+// Generically useful reading into a union type
+void readXYZ(int device,union XYZBuffer *xyz) {     
+  Wire.requestFrom(device, 6);      
+  long start=millis();
+  while (!Wire.available() && (millis()-start)<100);
+  if (millis()-start<100) {
+    for (int i=0;i<6;i++)
+      xyz->buff[i]=Wire.read();
+  }
+}
+
+void setupAccel(int device) {
+  // Check ID to see if we are communicating
+  Wire.beginTransmission(device);
+  Wire.write(0x00); // One Reading
+  Wire.endTransmission(); 
+  Wire.requestFrom(device,1);
+  while (!Wire.available());  
+  byte ch=Wire.read();
+  Serial.print("Accel id is 0x");
+  Serial.println(ch,HEX);
+  // Should output E5
+  
+  // https://www.sparkfun.com/datasheets/Sensors/Accelerometer/ADXL345.pdf
+  // Page 16
+  Wire.beginTransmission(device);
+  Wire.write(0x2d);
+  Wire.write(0x08);
+  Wire.endTransmission();
+  Wire.beginTransmission(device);
+  Wire.write(0x38);
+  Wire.write(0x84);
+  Wire.endTransmission();
+
+}
+void readAccel(int device,union XYZBuffer *xyz) {
+  Wire.beginTransmission(device);
+  Wire.write(0x32); // One Reading
+  Wire.endTransmission(); 
+  readXYZ(device,xyz);
+}
+
+void setupCompass(int device) {
+  // Check ID to see if we are communicating
+  Serial.print("Compass id is ");
+  Wire.beginTransmission(device);
+  Wire.write(10); // One Reading
+  Wire.endTransmission(); 
+  Wire.requestFrom(device,2); 
+  while (!Wire.available());
+  char ch=Wire.read();
+  Serial.print(ch);   
+  ch=Wire.read();
+  Serial.println(ch);
+  // Should output H4  
+  
+// Page 18
+// at http://dlnmh9ip6v2uc.cloudfront.net/datasheets/Sensors/Magneto/HMC5883L-FDS.pdf
+  Wire.beginTransmission(device);
+  Wire.write(0x00); Wire.write(0x70);
+  Wire.endTransmission();
+  Wire.beginTransmission(device);
+  Wire.write(0x01); Wire.write(0xA0);
+  Wire.endTransmission();
+  Wire.beginTransmission(device);
+  Wire.write(0x02); Wire.write(0x00); //  Reading
+  Wire.endTransmission(); 
+  delay(6);
+}
+void readCompass(int device,union XYZBuffer *xyz) {
+  readXYZ(device,xyz);
+  changeEndian(xyz);
+  Wire.beginTransmission(device);
+  Wire.write(0x03);
+  Wire.endTransmission(); 
+}
+
+void setupGyro(int device) {
+  // Check ID to see if we are communicating
+  Wire.beginTransmission(device);
+  Wire.write(0x00); // One Reading
+  Wire.endTransmission(); 
+  Wire.requestFrom(device,1);
+  while (!Wire.available());  
+  byte ch=Wire.read();
+  Serial.print("Gyro id is 0x");
+  Serial.println(ch,HEX);  
+  // Should output 69
+}
+void readGyro(int device,union XYZBuffer *xyz) {
+  // https://www.sparkfun.com/datasheets/Sensors/Gyro/PS-ITG-3200-00-01.4.pdf
+  // page 20
+  Wire.beginTransmission(device);
+  Wire.write(0x1d);
+  Wire.endTransmission(); 
+  readXYZ(device,xyz);
+  changeEndian(xyz);  
+}
+
+void pad(int width,int number) {
+  int n=abs(number);
+  int w=width;
+  if (number<0) w--;
+  while (n>0) {
+    w--;
+    n/=10;
+  }
+  if (number==0) w--;
+  for (int i=0;i<w;i++) Serial.print(' ');
+}
+
+void output(union XYZBuffer xyz) {
+//  pad(6,xyz.value.x);
+  Serial.print(xyz.value.x);
+  Serial.print(',');
+//  pad(6,xyz.value.y);
+  Serial.print(xyz.value.y);
+  Serial.print(',');
+//  pad(6,xyz.value.z);
+  Serial.print(xyz.value.z);
+}
+
+typedef struct {
+  int pin;
+  bool output; // input/output
+  bool analog; // analog/digital
+  bool pullup; // pullup/no pullup
+  int value;
+} io_t;
+
+int nios=0;
+io_t ios[8]; // at least as many as are setup()
+
+void setup()
+{
+  Serial.begin(9600);  // start serial for output
+  Wire.begin();        // join i2c bus (address optional for master)
+  setupCompass(COMPASSADDR);
+  setupAccel(ACCELADDR);
+  setupGyro(GYROADDR);
+
+  nois=0;
+  ios[nios].pin=13;
+  ios[nois].output=true;
+  ios[nois].analog=false;
+  ios[nois].pullup=false;
+  ios[nois].value=0;
+
+  ++nois;
+  ios[nios].pin = 8;
+  ios[nois].output = false;
+  ios[nois].analog = false;
+  ios[nois].pullup = true;
+  ios[nois].value = 0;
+
+  for (int i=0; i<nois; ++i) {
+    if (ios[i].output) {
+      if (ios[i].analog) {
+        analogWrite(ios[i].pin,ios[i].value);
+      } else {
+        digitalWrite(ios[i].pin,ios[i].value);
+        pinMode(ios[i],OUTPUT);
+      }
+    } else {
+      if (ios[i].analog) {
+        ios[i].value = analogRead(ios[i].pin);
+      } else {
+        if (ios[i].pullup) {
+          pinMode(ios[i],INPUT);
+          digitalWrite(ios[i],HIGH);
+        } else {
+          pinMode(ios[i],INPUT);
+        }
+	ios[i].value = digitalRead(ios[i].pin);
+      }
+    }
+  }
+}
+
+int state = 0;
+
+void proces(char ch)
+{
+  switch(state) {
+  case 0: 
+    if (ch == 'O') state = 1;
+    break;
+  case 1:
+    if (ch >= '1' && ch <= '4') {
+      index = (ch - '0');
+      state = 2;
+    } else {
+      state = 0;
+    }
+    break;
+  case 2:
+    if (ch == '=') {
+      state = 3;
+      number = 0;
+    } else {
+      state = 0;
+    }
+    break;
+  case 3:
+    if (ch == '\n' || ch == '\r') {
+      if (index < nios && ios[index].output) {
+        ios[index].value = number;
+        if (ios[index].analog) {
+          analogWrite(outputs[index].pin,number);
+        } else {
+          digitalWrite(outputs[index].pin,number);
+        }
+      }
+      state = 0;
+    } else if (ch >= '0' && ch <= '9') {
+      number = number*10 + (ch-'0');
+    } else {
+      state = 0;
+    }
+    break;
+  default:
+    state=0;
+  }
+}
+
+void read()
+{
+  while (Serial.available()) {
+    process(Serial.read());
+  }
+}
+
+void loop()
+{
+  union XYZBuffer compass,gyro,accel;
+  int l1,l2,l3,l4;
+
+  for (int i=0; i<nios; ++i) {
+    if (!ios[i].output) {
+      if (ios[i].analog) {
+        ios[i].value = analogRead(ios[i].pin);
+      } else {
+        ios[i].value = digitalRead(ios[i].pin);
+      }
+    }
+  }
+
+  l1=analogRead(0);
+  l2=analogRead(1);
+  l3=analogRead(2);
+  l4=analogRead(3);
+
+  while (Serial.available()) {
+
+  }
+  if (Serial.avail
+
+
+  readAccel(ACCELADDR,&accel);
+  readCompass(COMPASSADDR,&compass);
+  readGyro(GYROADDR,&gyro);
+  Serial.print("A,");
+  output(accel);
+  Serial.print(",C,");
+  output(compass);
+  Serial.print(",G,");  
+  output(gyro);
+  Serial.print(",L,");
+  Serial.print(l1);
+  Serial.print(",");
+  Serial.print(l2);
+  Serial.print(",");
+  Serial.print(l3);
+  Serial.print(",");
+  Serial.print(l4);
+  Serial.print(",I,");
+  Serial.print(i1);
+  Serial.print(",");
+  Serial.print(i2);
+  Serial.print(",");
+  Serial.print(i3);
+  Serial.print(",");
+  Serial.print(i4);
+  if (nios > 0) {
+    Serial.print(",S");
+    for (int i=0; i<nios; ++i) {
+      Serial.print(",");
+      Serial.print(ios[i].value);
+    }
+  }
+  Serial.println();
+}
--- centaur/sensors/src/main_sensors.cpp
+++ centaur/sensors/src/main_sensors.cpp
@@ -15,87 +15,80 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "CSVSplit.h"
+#include <condition_variable>
 
 using namespace std;
 
 Configure cfg;
 bool verbose;
-volatile bool running = true;
 
-void *context=0;
-void *pub=0;
-
-SensorsMessage sensors;
-double okRead;
-double readTimeout;
-double okReadTimeout;
-double okWrite;
-double okWriteTimeout;
-
-#define N 4096
-char buffer[N];
-
-int fd=-1;
-string line;
-
-void write_close()
+class Sensors
 {
-  if (pub != 0) {
-    zmq_close(pub);
-    pub = 0;
+private:
+  SensorsMessage data;
+  mutable std::mutex accessMutex;
+
+  void copyTo(SensorsMessage &dest) const
+  {
+    Lock lock(access);
+    memcpy(&dest,&data,sizeof(SensorsMessage));
   }
-}
 
-void zmqerror()
+  void copyFrom(const SensorsMessage &src)
+  {
+    Lock lock(access);
+    memcpy(&data,&src,sizeof(SensorsMessage));
+  }
+
+  Sensors()
+  {
+    memset(&data,0,sizeof(SensorsMessage));
+  }
+};
+
+Sensors current;
+
+class ZMQIO : ZMQHub
 {
-  int en=zmq_errno();
-  cout << "ZMQ error " 
-       << zmq_strerror(en) 
-       << " (" << en << ")" << endl;
-}
+  void init() {
+    rxPollTimeout = cfg.num(cfg.num("sensors.rxpolltimeout"));
+    rxTimeout = cfg.num(cfg.num("sensors.rxtimeout"));
+    txTimeout = cfg.num(cfg.num("sensors.txtimeout"));
+    subscribers = cfg.list("sensors.subscribers");
+    publish = cfg.str("sensors.publish");
+  }
 
-void write_open()
-{
-  write_close();
-
-  int rc = 0;
-  int hwm = 1;
-  
-  pub = zmq_socket(context,ZMQ_PUB);
-  rc = zmq_setsockopt(pub, ZMQ_SNDHWM, &hwm, sizeof(hwm));
-  if (rc != 0) {
-    zmqerror();
-    write_close();
-  } else {
-    rc = zmq_bind(pub, cfg.str("sensors.provider.publish").c_str());
-    if (rc!=0) {
-      zmqerror();
-      write_close();
+  void txWait()
+  {
+    std::unique_lock<std::mutex> lock(readyMutex);
+    for (;;) {
+      if (ready || !running) break;
+      readyCondition.wait_for(lock,1000/(2*rate));
     }
   }
 
-  okWrite = now();
-}
-
-void write()
-{
-  if (pub == 0 || okWrite+okWriteTimeout < now()) {
-    write_open();
+  void tx(ZMQPublishSocket &socket)
+  {
+    ZMQMessage msg(sizeof(SensorsMessage));
+    current.copyTo(msg.data());
+    msg.send();
+    ready=false;
   }
 
-  if (pub != 0) {
-    int rc = zmq_send(pub,&sensors, sizeof(SensorsMessage), ZMQ_DONTWAIT);
-    if (rc!=0) {
-    } else {
-      okWrite = sensors.t;
-    }
+  void tx()
+  {
+    tx_ready.signal();
   }
-}
+};
 
+     
 void report()
 {
+  SensorsMessage sensors;
+  current.copyTo(sensors);
+  
   cout << "t=" << sensors.t;
-
+  
   cout << " a=[" 
        << sensors.a[0] << ","
        << sensors.a[1] << ","
@@ -117,130 +110,208 @@ void report()
        << sensors.p[2] << ","
        << sensors.p[3] << "]";
   
+  cout << " s=["
+       << sensors.s[0] << ","
+       << sensors.s[1] << "]";
+  
   cout << endl;
 }
 
-void process(const std::string &line)
+class ArduinoIO
 {
-  sensors.t = now();
+  float timeout;
+  double ok;
+  int fd;
+  string line;
+  std::thread *go;
+  bool running = false;
+  float rate;
 
-  vector<string> vals;
-  CSVSplit(line,vals);
-  if (vals.size() == 17) {
-    bool ok = true;
-    int i = -1;
+  ArduinoIO()
+  {
+    rxReady=false;
+    go=0;
+  }
 
-    ok = ok && (vals[++i] == "A");
-    sensors.a[0]=atof(vals[++i].c_str());
-    sensors.a[1]=atof(vals[++i].c_str());
-    sensors.a[2]=atof(vals[++i].c_str());
+  void init()
+  {
+    rxReady=false;
+    go=0;
+    rate=cfg.num("sensors.rate");
+  }
 
-    ok = ok && (vals[++i] == "C");
-    sensors.c[0]=atof(vals[++i].c_str());
-    sensors.c[1]=atof(vals[++i].c_str());
-    sensors.c[2]=atof(vals[++i].c_str());
-
-    ok = ok && (vals[++i] == "G");
-    sensors.g[0]=atof(vals[++i].c_str());
-    sensors.g[1]=atof(vals[++i].c_str());
-    sensors.g[2]=atof(vals[++i].c_str());
-
-    ok = ok && (vals[++i] == "L");
-    sensors.p[0]=atof(vals[++i].c_str());
-    sensors.p[1]=atof(vals[++i].c_str());
-    sensors.p[2]=atof(vals[++i].c_str());
-    sensors.p[3]=atof(vals[++i].c_str());
-
-    if (ok) {
-      if (verbose) { 
-	report();
-      }
-      okRead = sensors.t;
-      write();
+  void start()
+  {
+    if (go == 0) {
+      go = new std::thread(ArduinoIO::update,this);
     }
   }
-}
 
-void process(char c)
-{
-  if (c == '\n') {
-    if (line.length() > 0) process(line);
-    line="";
-  } else if (c >= ' ') {
-    line.push_back(c);
-  }
-}
-
-void process(ssize_t n, char *c)
-{
-  for (ssize_t i = 0; i<n; ++i) {
-    process(c[i]);
-  }
-}
-
-void read_close()
-{
-  if (fd >= 0) close(fd);
-  fd = -1;
-}
-
-void read_open()
-{
-  read_close();
-  fd = open(cfg.str("sensors.provider.dev_path").c_str(),O_RDONLY|O_NONBLOCK);
-  if (verbose) {
-    cout << "open(" << cfg.str("sensors.provider.dev_path") << ")=" << fd << endl;
-  }
-  okRead = now();
-}
-
-ssize_t read_timeout(int fd, char *buffer, size_t n, size_t msTimeout)
-{
-  struct timespec ts;
-  ts.tv_sec = msTimeout/1000;
-  ts.tv_nsec = (msTimeout % 1000)*1000000;
-
-  fd_set set;
-  int rv;
-
-  FD_ZERO(&set);
-  FD_SET(fd, &set);
-
-  rv = pselect(fd + 1, &set, NULL, NULL, &ts, NULL);
-
-  if (rv > 0) {
-    return read(fd,buffer,n);
-  } else {
-    return 0;
-  }
-}
-
-void read()
-{
-  if (fd < 0 || okRead+okReadTimeout < now()) {
-    read_open();
+  void stop()
+  {
+    if (go != 0) {
+      running = false;
+      go->join();
+      delete go;
+      go = 0;
+    }
   }
 
-  ssize_t n = read_timeout(fd,buffer,N, 1000*readTimeout);
-  if (n > 0) process(n,buffer);
-}
+  void close()
+  {
+    if (fd >= 0) close(fd);
+    fd = -1;
+  }
+
+  void open()
+  {
+    close();
+    fd = open(cfg.str("sensors.dev_path").c_str(),O_NONBLOCK);
+    if (verbose) {
+      cout << "open(" << cfg.str("sensors.dev_path") << ")=" << fd << endl;
+    }
+    ok=now();
+  }
+
+  void write()
+  {
+    SensorMessage copy;
+    current.copyTo(copy);
+    char tmp[32];
+    snprintf(sizeof(tmp),tmp,"S0=%d\n",copy.s[0]);
+    write(fd,tmp,sizeof(tmp));
+  }
+
+  void process(const std::string &line)
+  {
+    SensorsMessage sesnors;
+
+    sensors.t = now();
+    
+    vector<string> vals;
+    CSVSplit(line,vals);
+    if (vals.size() == 20) {
+      bool ok = true;
+      int i = -1;
+      
+      ok = ok && (vals[++i] == "A");
+      sensors.a[0]=atof(vals[++i].c_str());
+      sensors.a[1]=atof(vals[++i].c_str());
+      sensors.a[2]=atof(vals[++i].c_str());
+      
+      ok = ok && (vals[++i] == "C");
+      sensors.c[0]=atof(vals[++i].c_str());
+      sensors.c[1]=atof(vals[++i].c_str());
+      sensors.c[2]=atof(vals[++i].c_str());
+      
+      ok = ok && (vals[++i] == "G");
+      sensors.g[0]=atof(vals[++i].c_str());
+      sensors.g[1]=atof(vals[++i].c_str());
+      sensors.g[2]=atof(vals[++i].c_str());
+      
+      ok = ok && (vals[++i] == "L");
+      sensors.p[0]=atof(vals[++i].c_str());
+      sensors.p[1]=atof(vals[++i].c_str());
+      sensors.p[2]=atof(vals[++i].c_str());
+      sensors.p[3]=atof(vals[++i].c_str());
+      
+      ok = ok && (vals[++i] == "S");
+      sensors.f[0]=atoi(vals[++i].c_str());
+      sensors.f[1]=atoi(vals[++i].c_str());
+      
+      if (ok) {
+	current.copyFrom(sensors);
+	if (zmqSensors.txSocket) {
+	  zmqSensors.tx(*zmqSensors.txSocket);
+	}
+	if (verbose) { 
+	  report();
+	}
+	okRead = sensors.t;
+      }
+    }
+  }
+
+  void process(char c)
+  {
+    if (c == '\n') {
+      if (line.length() > 0) process(line);
+      line="";
+    } else if (c >= ' ') {
+      line.push_back(c);
+    }
+  }
+
+  void process(ssize_t n, char *c)
+  {
+    for (ssize_t i = 0; i<n; ++i) {
+      process(c[i]);
+    }
+  }
+
+  ssize_t read_timeout(int fd, char *buffer, size_t n, size_t msTimeout)
+  {
+    struct timespec ts;
+    ts.tv_sec = msTimeout/1000;
+    ts.tv_nsec = (msTimeout % 1000)*1000000;
+    
+    fd_set set;
+    int rv;
+    
+    FD_ZERO(&set);
+    FD_SET(fd, &set);
+    
+    rv = pselect(fd + 1, &set, NULL, NULL, &ts, NULL);
+    
+    if (rv > 0) {
+      return ::read(fd,buffer,n);
+    } else {
+      return 0;
+    }
+  }
+
+  void read()
+  {
+    write();
+    char tmp[80];
+    size_t n=read_timeout(fd,tmp,sizeof(tmp),size_t(1000/(2*rate)));
+    if (n > 0) process(n,tmp);
+  }
+
+  void run()
+  {
+    open();
+    double txTime = now() + 1.0/rate;
+    while (running) {
+      if (txTime < now()) {
+	write();
+	txTime += 1.0/rate;
+      }
+      read();
+    }
+    close();
+  }
+};
+
+ArduinoIO arduinoIO;
+
+
+ZMQIO zmqIO;
 
 void quit(int sig)
 {
-  running = false;
+  zmqSensors->stop();
+  zmqSensors->join();
+  arduinoIO->stop();
 }
 
 void config(int argc, char** argv)
 {
   cfg.path("../../setup");
-  cfg.args("sensors.provider.", argv);
+  cfg.args("sensors.", argv);
   if (argc == 1) cfg.load("config.csv");
-  verbose = cfg.flag("sensors.provider.verbose", false);
+  verbose = cfg.flag("sensors.verbose", false);
   if (verbose) cfg.show();
-
-  readTimeout = cfg.num("sensors.provider.readtimeout");
-  okReadTimeout = cfg.num("sensors.provider.okreadtimeout");
-  okWriteTimeout = cfg.num("sensors.provider.okwritetimeout");
 }
 
 int main(int argc, char** argv)
@@ -251,17 +322,9 @@ int main(int argc, char** argv)
   signal(SIGTERM, quit);
   signal(SIGQUIT, quit);
 
-  context = zmq_ctx_new ();
-
-
-  while(running) { 
-    read();
-  }
-
-  read_close();
-  write_close();
-
-  zmq_ctx_destroy(context);
+  arduinoIO.start();
+  zmqIO.start();
+  zmqIO.join();
   
   return 0;
 }
