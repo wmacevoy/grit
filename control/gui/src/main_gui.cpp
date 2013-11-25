ad5b0e63a4d6867f014bee5167e051ab6ce33345
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-11-25 15:28:44 +0000

    trial
    Former-commit-id: 23c19679396b63c1a872233621102eb775a6be6f

--- control/gui/src/main_gui.cpp
+++ control/gui/src/main_gui.cpp
@@ -25,15 +25,14 @@ string NumberToString ( T Number )
   return ss.str();
 }
 
-class RobotWatcher
+class RobotWatcher : public Gtk::Window
 {
-private:
+protected:
 	Glib::RefPtr<Gtk::Builder> builder;
-	Gtk::Window *w;
-	Gtk::Main kit;
 	Gtk::ColorButton *temp_button[200];
-	Gtk::ColorButton *pressure_button[4];
+	Gtk::ColorButton *pressure_button[5];
 	Gdk::Color sev_colors[4];
+	Gtk::Label *lblTop, *lblTemp;
 	void *temp_context;
 	void *sensor_context;
 	void *temp_sub;
@@ -47,26 +46,17 @@ private:
 	int32_t temps[NUM_SERVOS];
 	
 public:
-	RobotWatcher(int argc, char *argv[])
-		: kit(argc, argv)
+	RobotWatcher(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade) : Gtk::Window(cobject), builder(refGlade)
 	{
 		hwm = 1;
 		linger = 25;
 		t1=0;
 		t2=0;
 		timeOut=0.5;
-
-		cfg.path("../../setup");
-		cfg.args("gui.requester.", argv);
-		if (argc == 1) cfg.load("config.csv");
-		verbose = cfg.flag("gui.requester.verbose", false);
-		if (verbose) cfg.show();
 		
 		address_t = cfg.str("gui.requester.address_t").c_str();
 		address_s = cfg.str("gui.requester.address_s").c_str();
 		SLEEP_TIME = (int)(cfg.num("gui.requester.sleep_t"));
-
-		builder = Gtk::Builder::create();
 		
 		temp_context = zmq_ctx_new ();
 		temp_sub = zmq_socket(temp_context, ZMQ_SUB);
@@ -95,114 +85,11 @@ public:
 		sev_colors[3].set_rgb(0,0,0);
 
 		for (int i = 0; i < NUM_SERVOS; i++)
-		  temps[i]=0;
+		  temps[i]=100;
 
 		for (int i = 0; i < NUM_SENSORS; i++)
 		  sensors[i] = 0;
-	}
 
-	~RobotWatcher(){}
-
-	bool subscribe_temperatures(int32_t temps[])
-	{
-	  bool ret = false;
-		zmq_msg_t msg;
-		zmq_msg_init (&msg);
-		
-		if(zmq_recvmsg(temp_sub, &msg, ZMQ_DONTWAIT) > 0) {
-			memcpy(temps, zmq_msg_data(&msg), zmq_msg_size(&msg));			
-			ret = true;
-			t1=now();
-		}
-		zmq_msg_close(&msg);	
-
-		t2 = now();
-		if(t2 - t1 > timeOut) {
-			zmq_close(temp_sub);
-			temp_sub = zmq_socket(temp_context, ZMQ_SUB);
-			if(zmq_setsockopt(temp_sub, ZMQ_SUBSCRIBE, "", 0) == 0) {
-				if(zmq_setsockopt(temp_sub, ZMQ_RCVHWM, &hwm, sizeof(hwm)) == 0) {
-					if(zmq_setsockopt(temp_sub, ZMQ_SNDHWM, &hwm, sizeof(hwm)) == 0) {
-						if(zmq_setsockopt(temp_sub, ZMQ_LINGER, &linger, sizeof(linger)) == 0) {
-							rcc = zmq_connect(temp_sub, address_t.c_str());
-						}
-					}
-				}
-			}
-		}
-		return ret;
-	}
-
-	bool subscribe_sensors(int32_t sensors[])
-	{
-	  bool ret = false;
-		zmq_msg_t msg;
-		zmq_msg_init (&msg);
-		
-		if(zmq_recvmsg(sensor_sub, &msg, ZMQ_DONTWAIT) > 0) {
-			memcpy(sensors, zmq_msg_data(&msg), zmq_msg_size(&msg));		
-			ret = true;
-			t1=now();
-		}
-		zmq_msg_close(&msg);	
-
-		t2 = now();
-		if(t2 - t1 > timeOut) {
-			zmq_close(sensor_sub);
-			sensor_sub = zmq_socket(sensor_context, ZMQ_SUB);
-			if(zmq_setsockopt(sensor_sub, ZMQ_SUBSCRIBE, "", 0) == 0) {
-				if(zmq_setsockopt(sensor_sub, ZMQ_RCVHWM, &hwm, sizeof(hwm)) == 0) {
-					if(zmq_setsockopt(sensor_sub, ZMQ_SNDHWM, &hwm, sizeof(hwm)) == 0) {
-						if(zmq_setsockopt(sensor_sub, ZMQ_LINGER, &linger, sizeof(linger)) == 0) {
-							rcc = zmq_connect(sensor_sub, address_s.c_str());
-						}
-					}
-				}
-			}
-		}
-		return ret;
-	}
-
-	void update_colors_temps(int32_t temps[], int size)
-	{
-		int sev = 3;
-		for (int i = 0; i < size; i+=2)
-		{
-			if (temps[i+1] > 65)
-				sev = 0;
-			else if (temps[i+1] > 45)
-				sev = 1;
-			else if (temps[i+1] > 0)
-				sev = 2;
-			else
-				sev = 3;
-
-			temp_button[temps[i]]->set_color(sev_colors[sev]);
-		}
-	}
-
-	void update_colors_pressure(int32_t sensors[], int size) 
-	{
-		int sev = 3;
-		for(int i = 10; i < size; i++)
-		{
-			if (sensors[i] > 900)
-				sev = 0;
-			else if (sensors[i] > 750)
-				sev = 1;
-			else if (sensors[i] >= 0)
-				sev = 2;
-			else
-				sev = 3;
-
-			pressure_button[i]->set_color(sev_colors[sev]);
-		}
-	}
-
-	void init()
-	{
-		builder->add_from_file("main.xml");
-		builder->get_widget("winStatus",w);
 		for (int i =11; i < 14; i++)
 		{
 			string btn_string = "sig" + NumberToString(i);
@@ -257,10 +144,117 @@ public:
 			builder->get_widget(btn_string.c_str(),pressure_button[i]);
 			//temp_button[i]->set_sensitive(false);			
 		}
+		builder->get_widget("lblTop", lblTop);
+		builder->get_widget("lblTemp", lblTemp);
+	
+		lblTop->set_text("Top: N/A");
 
-		w->signal_delete_event().connect(sigc::mem_fun(this, &RobotWatcher::end1));
-		Glib::signal_timeout().connect(sigc::mem_fun(this, &RobotWatcher::on_timer), SLEEP_TIME );
+		Glib::signal_timeout().connect(sigc::mem_fun(*this, &RobotWatcher::on_timer), SLEEP_TIME );
 
+	}
+
+	~RobotWatcher(){}
+
+	bool subscribe_temperatures(int32_t temps[])
+	{
+	  bool ret = false;
+		zmq_msg_t msg;
+		zmq_msg_init (&msg);
+		
+		if(zmq_recvmsg(temp_sub, &msg, ZMQ_DONTWAIT) > 0) {
+			memcpy(temps, zmq_msg_data(&msg), zmq_msg_size(&msg));			
+			ret = true;
+			t1=now();
+			for(int i=0; i < NUM_SERVOS; ++i) std::cout << temps[i] << " ";
+			std::cout << std::endl;
+		}
+		zmq_msg_close(&msg);	
+
+		t2 = now();
+		if(t2 - t1 > timeOut) {
+			zmq_close(temp_sub);
+			temp_sub = zmq_socket(temp_context, ZMQ_SUB);
+			if(zmq_setsockopt(temp_sub, ZMQ_SUBSCRIBE, "", 0) == 0) {
+				if(zmq_setsockopt(temp_sub, ZMQ_RCVHWM, &hwm, sizeof(hwm)) == 0) {
+					if(zmq_setsockopt(temp_sub, ZMQ_SNDHWM, &hwm, sizeof(hwm)) == 0) {
+						if(zmq_setsockopt(temp_sub, ZMQ_LINGER, &linger, sizeof(linger)) == 0) {
+							rcc = zmq_connect(temp_sub, address_t.c_str());
+						}
+					}
+				}
+			}
+		}
+		return ret;
+	}
+
+	bool subscribe_sensors(int32_t sensors[])
+	{
+	  bool ret = false;
+		zmq_msg_t msg;
+		zmq_msg_init (&msg);
+		
+		if(zmq_recvmsg(sensor_sub, &msg, ZMQ_DONTWAIT) > 0) {
+			memcpy(sensors, zmq_msg_data(&msg), zmq_msg_size(&msg));		
+			ret = true;
+			t1=now();
+			for(int i=0; i < NUM_SENSORS; ++i) std::cout << sensors[i] << " ";
+			std::cout << std::endl;
+		}
+		zmq_msg_close(&msg);	
+
+		t2 = now();
+		if(t2 - t1 > timeOut) {
+			zmq_close(sensor_sub);
+			sensor_sub = zmq_socket(sensor_context, ZMQ_SUB);
+			if(zmq_setsockopt(sensor_sub, ZMQ_SUBSCRIBE, "", 0) == 0) {
+				if(zmq_setsockopt(sensor_sub, ZMQ_RCVHWM, &hwm, sizeof(hwm)) == 0) {
+					if(zmq_setsockopt(sensor_sub, ZMQ_SNDHWM, &hwm, sizeof(hwm)) == 0) {
+						if(zmq_setsockopt(sensor_sub, ZMQ_LINGER, &linger, sizeof(linger)) == 0) {
+							rcc = zmq_connect(sensor_sub, address_s.c_str());
+						}
+					}
+				}
+			}
+		}
+		return ret;
+	}
+
+	void update_colors_temps(int32_t temps[], int size)
+	{
+		int sev = 3, max = 0;
+		for (int i = 0; i < size; i+=2)
+		{
+			if(temps[i+1] > max) max = temps[i+1];
+			if (temps[i+1] > 65)
+				sev = 0;
+			else if (temps[i+1] > 45)
+				sev = 1;
+			else if (temps[i+1] > 0)
+				sev = 2;
+			else
+				sev = 3;
+
+			temp_button[temps[i]]->set_color(sev_colors[sev]);
+			lblTop->set_text("Top: " + NumberToString(temps[i+1]));
+		}
+	}
+
+	void update_colors_pressure(int32_t sensors[], int size) 
+	{
+		int sev = 3;
+		for(int i = 10; i < size; i++)
+		{
+			if (sensors[i] > 900)
+				sev = 0;
+			else if (sensors[i] > 750)
+				sev = 1;
+			else if (sensors[i] >= 0)
+				sev = 2;
+			else
+				sev = 3;
+
+			pressure_button[i]->set_color(sev_colors[sev]);
+		}
 	}
 
 	bool on_timer()
@@ -272,14 +266,7 @@ public:
 		got = subscribe_sensors(sensors);
 		if (got)
 		  update_colors_pressure(sensors, NUM_SENSORS);
-		
-		return true;
-	}
 
-	//X button on window is clicked
-	bool end1(GdkEventAny *)
-	{
-		Gtk::Main::quit();  
 		return true;
 	}
 
@@ -289,16 +276,9 @@ public:
 		Gtk::Main::quit();
 	}
 
-	void run()
-	{
-	  kit.run(*w);
-	  end();
-	}
-
 	void end()
 	{
 		std::cout << std::endl << "Quitting..." << std::endl;
-		delete w;
 		zmq_close(temp_sub);
 		zmq_close(sensor_sub);
 		zmq_ctx_destroy(temp_context);
@@ -315,13 +295,25 @@ void quitproc(int param) {
 
 int main(int argc, char *argv[])
 {
+	cfg.path("../../setup");
+	cfg.args("gui.requester.", argv);
+	if (argc == 1) cfg.load("config.csv");
+	verbose = cfg.flag("gui.requester.verbose", false);
+	if (verbose) cfg.show();
+	
 	signal(SIGINT, quitproc);
 	signal(SIGTERM, quitproc);
 	signal(SIGQUIT, quitproc);
 
-	RobotWatcher gui(argc,argv);
-	r = &gui;
-	gui.init();
-	gui.run();
+	Gtk::Main kit(argc,argv);
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("main.xml");
+
+	RobotWatcher *gui = 0;
+	r = gui;
+	builder->get_widget_derived("winStatus", gui);
+	kit.run(*gui);
+	gui->end();
+
+
 	return 0;
 }
