ec16b7337884ec86c2b440924079d4fe79cdbdba
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-12-02 14:09:53 +0000

    - in drivers
    Former-commit-id: 95cfd73c9718876f1f9ab884b0a96ed9f54a2969

--- drivers/3d/src/test_arc.cpp
+++ drivers/3d/src/test_arc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 #include "Vec3d.h"
 #include "Arc.h"
@@ -57,9 +58,77 @@ void test2()
   }
 }
 
+void test3()
+{
+  Mat3d m=Mat3d::identity();
+
+  m=m*translate(Vec3d(10,30,15));
+  m=m*rotate(Vec3d::o,Vec3d::ex,90-15);
+  
+  cout << "m=" << m << endl;
+  Vec3d p1=m*Vec3d(15*cos(M_PI/180*30),15*sin(M_PI/180*30),0);
+  Vec3d p2=m*Vec3d(15*cos(M_PI/180*0),15*sin(M_PI/180*0),0);
+  Vec3d p3=m*Vec3d(15*cos(M_PI/180*(-30)),15*sin(M_PI/180*(-30)),0);
+
+  cout << "p1=" << p1 << " p2=" << p2 << " p3=" << p3 << endl;
+  Arc arc;
+
+  arc.fromPoints(p1,p2,p3);
+
+  int n=10;
+  cout << "s,x,y,z,px,py,pz,ux,uy,uz" << endl;
+  for (int i=0; i<=n; ++i) {
+    float s=float(i)/float(n);
+    Mat3d p=arc.pose(s);
+    Vec3d at=p.o();
+    Vec3d point=p.ey();
+    Vec3d up=p.ez();
+    cout << s 
+	 << "," << at(0) << "," << at(1) << "," << at(2) 
+	 << "," << point(0) << "," << point(1) << "," << point(2)
+	 << "," << up(0) << "," << up(1) << "," << up(2)
+	 << endl;
+  }
+}
+
+void test4()
+{
+  Mat3d m=Mat3d::identity();
+
+  m=m*translate(Vec3d(10,30,15));
+  m=m*rotate(Vec3d::o,Vec3d::ex,90-15);
+  
+  cout << "m=" << m << endl;
+  Vec3d p1=m*Vec3d(15*cos(M_PI/180*30),15*sin(M_PI/180*(-30)),0);
+  Vec3d p2=m*Vec3d(15*cos(M_PI/180*0),15*sin(M_PI/180*0),0);
+  Vec3d p3=m*Vec3d(15*cos(M_PI/180*(-30)),15*sin(M_PI/180*(30)),0);
+
+  cout << "p1=" << p1 << " p2=" << p2 << " p3=" << p3 << endl;
+  Arc arc;
+
+  arc.fromPoints(p1,p2,p3);
+
+  int n=10;
+  cout << "s,x,y,z,px,py,pz,ux,uy,uz" << endl;
+  for (int i=0; i<=n; ++i) {
+    float s=float(i)/float(n);
+    Mat3d p=arc.pose(s);
+    Vec3d at=p.o();
+    Vec3d point=p.ey();
+    Vec3d up=p.ez();
+    cout << s 
+	 << "," << at(0) << "," << at(1) << "," << at(2) 
+	 << "," << point(0) << "," << point(1) << "," << point(2)
+	 << "," << up(0) << "," << up(1) << "," << up(2)
+	 << endl;
+  }
+}
+
 int main()
 {
-  test1();
-  test2();
+  //  test1();
+  //  test2();
+  //  test3();
+  test4();
   return 0;
 }
