6f42d319e8f2a848c519667beb34acc9658c9a0e
warren <warren@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2013-12-05 16:48:45 +0000

    ods root for servomap
    Former-commit-id: 0e3165bc682ac1070ee90a5564cbe0623d18a41f

--- setup/setup_dev_environment.sh
+++ setup/setup_dev_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost-all-dev cmake libusb-1.0-0-dev swig python-dev scilab bison flex libgtkmm-2.4-dev libarmadillo-dev
+apt-get install build-essential debian-keyring g++-multilib g++-4.6-multilib gcc-4.6-doc libstdc++6-4.6-dbg libstdc++6-4.6-doc libboost-all-dev cmake libusb-1.0-0-dev swig python-dev scilab bison flex libgtkmm-2.4-dev libarmadillo-dev unoconv
+
 
 
