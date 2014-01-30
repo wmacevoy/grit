67668a1127a1929ad80b0c7153b8dfafdb90940b
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2014-01-30 04:06:03 +0000

    logo this
    Former-commit-id: 3324db7fee211ae877520744fc3cd0197e5565c3

--- haarkit/tools/traincascade.sh
+++ haarkit/tools/traincascade.sh
@@ -1,1 +1,1 @@
-./opencv_traincascade -data data/ -vec data/vector.vec -bg negative/negatives.txt -numPos 1000 -numNeg 7 -numStages 13 -featureType HAAR -w 40 -h 40 -bt GAB -maxFalseAlarmRate 0.3 -mode ALL
+./opencv_traincascade -data data/ -vec data/vector.vec -bg negative/negatives.txt -numPos 1000 -numNeg 7 -numStages 13 -featureType HAAR -bt GAB -minHitRate 0.999 -maxFalseAlarmRate 0.5 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -w 40 -h 40 -mode ALL
