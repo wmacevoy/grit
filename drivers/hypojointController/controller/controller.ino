2cb11c966fdc81f2fa936ae47e42ddbb9fa6997b
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2014-02-15 16:08:08 +0000

    finally got default.h to show up, moved structs there
    Former-commit-id: 403c1b3b7fdcb71154547a12d989ede5824a3ee0

--- drivers/hypojointController/controller/controller.ino
+++ drivers/hypojointController/controller/controller.ino
@@ -11,7 +11,7 @@
 #include <Wire.h>
 #include <EEPROM.h>
 #include "EEPROMAnything.h"
-//#include "default.hpp"
+#include "default.h"
 
 #define WIDTH  (8 * sizeof(crc))
 #define TOPBIT (1 << (WIDTH - 1))
@@ -46,57 +46,6 @@ int  position = 0;
 int  dir      = 0;          // 1 counter clockwise/0 stop/-1 clockwise
 int  wait     = 100;
 int  addr     = 0;          //eeprom memory address
-
-struct Id{
- byte val;
- static const limit = 255;
- static const int address = 0;
- Id(){val = 0;}
-}_id;
-struct PotPin{
- int val;
- static const int address = 1;
- PotPin(){val = A3;}
-}_potPin; 
-struct DirPin{
- int val;
- static const int address = 3;
- DirPin(){val = 8;}
-}_dirPin;
-struct StepPin{
- int val;
- static const int address = 5;
- StepPin(){val = 9;}
-}_stepPin;
-struct LedPin{
- int val;
- static const int address = 7;
- LedPin(){val = 13;}
-}_ledPin;
-struct MinFreq{
- int val;
- static const limit = 0;
- static const int address = 9; 
- MinFreq(){val = 0;}
-}_minFreq;
-struct MaxFreq{
- long val;
- static const long limit = 3500;
- static const int address = 11;
- MaxFreq(){val = 3500;}
-}_maxFreq;
-struct MinPos{
- int val;
- static const int limit = 0;
- static const int address = 15;
- MinPos(){val = 0;}
-}_minPos;
-struct MaxPos{
- long val;
- static const long limit = 1024;
- static const int address = 17;
- MaxPos(){val = 1024;}
-}_maxPos;
 
 struct Response{
   int pos;
---
+++ drivers/hypojointController/controller/default.h
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+
+struct Id{
+ byte val;
+ static const byte limit = 255;
+ static const int address = 0;
+ Id(){val = 0;}
+}_id;
+struct PotPin{
+ int val;
+ static const int address = 1;
+ PotPin(){val = A3;}
+}_potPin; 
+struct DirPin{
+ int val;
+ static const int address = 3;
+ DirPin(){val = 8;}
+}_dirPin;
+struct StepPin{
+ int val;
+ static const int address = 5;
+ StepPin(){val = 9;}
+}_stepPin;
+struct LedPin{
+ int val;
+ static const int address = 7;
+ LedPin(){val = 13;}
+}_ledPin;
+struct MinFreq{
+ int val;
+ static const int limit = 0;
+ static const int address = 9; 
+ MinFreq(){val = 0;}
+}_minFreq;
+struct MaxFreq{
+ long val;
+ static const long limit = 3500;
+ static const int address = 11;
+ MaxFreq(){val = 3500;}
+}_maxFreq;
+struct MinPos{
+ int val;
+ static const int limit = 0;
+ static const int address = 15;
+ MinPos(){val = 0;}
+}_minPos;
+struct MaxPos{
+ long val;
+ static const long limit = 1024;
+ static const int address = 17;
+ MaxPos(){val = 1024;}
+}_maxPos;
