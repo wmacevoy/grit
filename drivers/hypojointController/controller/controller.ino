a77b76bddb844f81d8028bb331e69a9b66e4c6fe
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2014-02-15 15:02:22 +0000

    added limits to some structs
    Former-commit-id: 7ff78156ce8bb577a3c954a17aca610539fefaa6

--- drivers/hypojointController/controller/controller.ino
+++ drivers/hypojointController/controller/controller.ino
@@ -49,6 +49,7 @@ int  addr     = 0;          //eeprom memory address
 
 struct Id{
  byte val;
+ static const limit = 255;
  static const int address = 0;
  Id(){val = 0;}
 }_id;
@@ -74,21 +75,25 @@ struct LedPin{
 }_ledPin;
 struct MinFreq{
  int val;
+ static const limit = 0;
  static const int address = 9; 
  MinFreq(){val = 0;}
 }_minFreq;
 struct MaxFreq{
  long val;
+ static const long limit = 3500;
  static const int address = 11;
  MaxFreq(){val = 3500;}
 }_maxFreq;
 struct MinPos{
  int val;
+ static const int limit = 0;
  static const int address = 15;
  MinPos(){val = 0;}
 }_minPos;
 struct MaxPos{
  long val;
+ static const long limit = 1024;
  static const int address = 17;
  MaxPos(){val = 1024;}
 }_maxPos;
