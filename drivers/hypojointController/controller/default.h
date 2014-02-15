e14d20b7f11bbd7cdd2b6a31ceecc4ad00cbd317
michaela <michaela@9d61a3f9-a2cd-437f-8723-061d0b64babd>
2014-02-15 16:21:42 +0000

    readability
    Former-commit-id: ccc3bf2ff53daeb9521279491e0ded9a7931126d

--- drivers/hypojointController/controller/default.h
+++ drivers/hypojointController/controller/default.h
@@ -6,44 +6,52 @@ struct Id{
  static const int address = 0;
  Id(){val = 0;}
 }_id;
+
 struct PotPin{
  int val;
  static const int address = 1;
  PotPin(){val = A3;}
 }_potPin; 
+
 struct DirPin{
  int val;
  static const int address = 3;
  DirPin(){val = 8;}
 }_dirPin;
+
 struct StepPin{
  int val;
  static const int address = 5;
  StepPin(){val = 9;}
 }_stepPin;
+
 struct LedPin{
  int val;
  static const int address = 7;
  LedPin(){val = 13;}
 }_ledPin;
+
 struct MinFreq{
  int val;
  static const int limit = 0;
  static const int address = 9; 
  MinFreq(){val = 0;}
 }_minFreq;
+
 struct MaxFreq{
  long val;
  static const long limit = 3500;
  static const int address = 11;
  MaxFreq(){val = 3500;}
 }_maxFreq;
+
 struct MinPos{
  int val;
  static const int limit = 0;
  static const int address = 15;
  MinPos(){val = 0;}
 }_minPos;
+
 struct MaxPos{
  long val;
  static const long limit = 1024;
