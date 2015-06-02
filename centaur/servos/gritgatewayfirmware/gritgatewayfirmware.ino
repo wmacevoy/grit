effc7a8edb54bebee3eeecf837e72bf705d9811c
Warren MacEvoy <wmacevoy@gmail.com>
2015-06-01 21:48:27 -0700

    messages

--- centaur/servos/gritgatewayfirmware/gritgatewayfirmware.ino
+++ centaur/servos/gritgatewayfirmware/gritgatewayfirmware.ino
@@ -52,8 +52,13 @@ void setup()
   }
 }
 
+int lastState = -1;
+
 void loop() 
 {
+  if (state != lastState) {
+    lastState = state;
+  }
   if (state != 0 && (long(millis())-timeout) > 0) {
     state = 0;
   }
@@ -66,7 +71,7 @@ void loop()
     }
     break;
   case 1:
-    if (ch(address) && (address > 0 && address < 127)) {
+    if (ch(address) && (address >= 0 && address <= 127)) {
 	timeout=millis()+TIMEOUT;
 	state = 2;
     }
@@ -75,13 +80,14 @@ void loop()
     if (ch(value)) {
         address=40;
         value=66;
+
         Wire.beginTransmission(address);
         Wire.write(value);
         Wire.endTransmission();
-        Serial.println("sent.");
 	if (command == 'g') {
           Wire.requestFrom((int)address, 1);
   	  timeout=millis()+TIMEOUT;
+          Serial.print(value); Serial.print("@"); Serial.println(address);
 	  state = 3;
 	} else {
 	  state = 0;
