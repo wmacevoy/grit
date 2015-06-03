b2423d709d25b053c72ef0f0eb47d12b7270712d
Michaela Ervin <ts.michaela.elise@gmail.com>
2015-06-03 12:48:39 -0700

    added low res requester

---
+++ control/webcamRequester2/src/main_webcamRequester.cpp
@@ -0,0 +1,116 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <signal.h>
+#include <sstream>
+#include <string>
+#include "Configure.h"
+#include "robocam.h"
+
+using namespace cv;
+
+Configure cfg;
+
+RobotWatcher *ghost;
+
+volatile bool die = false;
+bool inside = false;
+bool verbose = false;
+
+template<class T>
+std::string itoa(const T& t) {
+	std::stringstream itoa;
+	itoa << t;
+	return itoa.str();
+}
+
+void quitproc(int param) {
+	die = true;
+	ghost->kill();
+}
+
+void mouseEvent(int evt, int x, int y, int flags, void* param) {
+	/*if(evt == CV_EVENT_MOUSEMOVE) {
+		ghost->setMouse(x, y);
+	}*/
+}
+
+int main(int argc, char** argv)
+{
+	cfg.path("../../setup");
+	cfg.args("webcam.requester2.", argv);
+	if (argc == 1) cfg.load("config.csv");
+	verbose = cfg.flag("webcam.requester2.verbose", false);
+	if (verbose) cfg.show();
+
+	int sleep_time = cfg.num("webcam.requester2.sleep_time");
+	int port = (int)cfg.num("webcam.provider2.port");
+	//std::string lidarAddress = cfg.str("lidar.provider.subscribe");
+	//bool lidarCalibration = cfg.flag("detector.webcam.requester.calibration", false);
+
+
+	RobotWatcher my_watcher;
+	ghost = &my_watcher;
+	
+	int imgNum = 0;
+	bool receiving = true;
+	Mat frame;
+	int t1 = 0, t2 = 0;
+
+	std::string windowName = "ICU (LOW)";
+	std::string imageName = "";
+	namedWindow(windowName, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_NORMAL);
+
+	signal(SIGINT, quitproc);
+	signal(SIGTERM, quitproc);
+	signal(SIGQUIT, quitproc);
+	
+	//Initialize watcher
+	my_watcher.setup(port, true, verbose);
+	//my_watcher.setupLidar(lidarAddress, lidarCalibration, verbose);
+
+	//cvSetMouseCallback(windowName.c_str(), mouseEvent, 0);
+
+	while(!die) {
+		if(receiving) {
+        try
+          {
+		  frame = my_watcher.grab_image();
+          }
+        catch(int e)
+          {
+          std::cout << "No data available!" << std::endl;
+          }
+
+        if(!frame.empty())
+         {
+		 imshow(windowName, frame);
+         }
+		}
+
+		char c = waitKey(sleep_time);
+		if(c == 'q') die = true;
+		else if(c == 's') {
+			imageName = "image";
+			imageName += itoa(imgNum++);
+			imageName += ".jpg";
+			imwrite(imageName,  frame);
+		}
+		else if(c == 'p') {
+			receiving = !receiving;
+		}
+		else if(c =='u') {
+			if(sleep_time < 10000) sleep_time += 100;
+		}
+		else if(c =='t') {
+			if(sleep_time > 100)  sleep_time -= 100;
+		}
+	}
+
+	std::cout << std::endl << "Quitting..." << std::endl;
+	std::cout << "destroying window and freeing mat memory..." << std::endl;
+	destroyWindow(windowName);
+	frame.release();
+	std::cout << "--done!" << std::endl;
+	return 0;
+}
